c8f2d861fc8d6d3bd99b62ee88b2c2e11b183f19
Yichao Yu <yyc1992@gmail.com>
2022-09-27 20:12:47 -0400

    Handle aborted sequence

--- lib/NaCsData/src/load_artiq.jl
+++ lib/NaCsData/src/load_artiq.jl
@@ -15,12 +15,28 @@ function load_dax_scan1(fd)
     params = first(values(scan))
     nparams = length(params)
     pmt_counts = read(fd, "datasets/histogram_context/histogram/raw")
-    @assert nparams == length(pmt_counts)
-    counts = Matrix{Int}(undef, nparams, 2)
-    for i in 1:nparams
-        pmt_count = pmt_counts[string(i - 1)]
-        counts[i, 1] = length(pmt_count)
-        counts[i, 2] = count(>=(thresh), pmt_count)
+    actual_nparams = length(pmt_counts)
+    if nparams == actual_nparams
+        counts = Matrix{Int}(undef, nparams, 2)
+        for i in 1:nparams
+            pmt_count = pmt_counts[string(i - 1)]
+            counts[i, 1] = length(pmt_count)
+            counts[i, 2] = count(>=(thresh), pmt_count)
+        end
+    else
+        counts = Matrix{Int}(undef, actual_nparams, 2)
+        new_params = similar(params, nparams)
+        idx = 0
+        for i in 1:nparams
+            k = string(i - 1)
+            (k in keys(pmt_counts)) || continue
+            idx += 1
+            new_params[idx] = params[i]
+            pmt_count = pmt_counts[k]
+            counts[idx, 1] = length(pmt_count)
+            counts[idx, 2] = count(>=(thresh), pmt_count)
+        end
+        params = new_params
     end
     return (param_name,), CountData(params, counts)
 end
