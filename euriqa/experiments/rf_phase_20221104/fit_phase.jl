426220eb6d9ea0aac279333d0dd32fb1b63dec2a
Yichao Yu <yyc1992@gmail.com>
2022-11-04 21:59:11 -0400

    [euriqa/experiments/rf_phase_20221104] WIP: pre-shrink blocks

--- euriqa/experiments/rf_phase_20221104/fit_phase.jl
+++ euriqa/experiments/rf_phase_20221104/fit_phase.jl
@@ -7,6 +7,14 @@ load_data(fname) = h5open(fname) do io
             tscale=read(io, "tscale"), vscale=read(io, "vscale"))
 end
 
+function shrink_block(block, r_start, r_end)
+    i = first(block)
+    l = last(block)
+    new_i = round(Int, i * (1 - r_start) + l * r_start)
+    new_l = round(Int, i * (1 - r_end) + l * r_end)
+    return new_i:new_l
+end
+
 function find_blocks(data)
     threshold = 20
     min_size = 50
@@ -23,7 +31,7 @@ function find_blocks(data)
             end
             if i > last_above + end_threshold
                 if last_above - block_start > min_size
-                    push!(blocks, (block_start, last_above))
+                    push!(blocks, shrink_block(block_start:last_above, 0.1, 0.9))
                 end
                 in_block = false
             end
@@ -36,16 +44,22 @@ function find_blocks(data)
         end
     end
     if in_block && last_above - block_start > min_size
-        push!(blocks, (block_start, last_above))
+        push!(blocks, shrink_block(block_start:last_above, 0.1, 0.9))
     end
 
     return blocks
 end
 
-function shrink_block(block, r_start, r_end)
-    i = first(block)
-    l = last(block)
-    new_i = round(Int, i * (1 - r_start) + l * r_start)
-    new_l = round(Int, i * (1 - r_end) + l * r_end)
-    return new_i:new_l
+function find_zeros(ts, data, trigger=0)
+    crossings = Float64[]
+    for i in 1:length(data) - 1
+        d1 = data[i]
+        d2 = data[i + 1]
+        if d1 <= 0 && d2 > 0
+            t1 = ts[i]
+            t2 = ts[i + 1]
+            push!(crossings, (t1 * d2 - t2 * d1) / (d2 - d1))
+        end
+    end
+    return crossings
 end
