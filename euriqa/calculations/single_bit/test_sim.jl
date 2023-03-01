5164299f567362e3e0e90b5c0fd901f7a5b4704b
Yichao Yu <yyc1992@gmail.com>
2023-02-28 22:37:43 -0500

    [euriqa/calculation/single_bit] xy rotation

---
+++ euriqa/calculations/single_bit/test_sim.jl
@@ -0,0 +1,12 @@
+#!/usr/bin/julia
+
+function xy_rotation(ψ, θ)
+    #   e^(i * (σ_x cosθ + σ_y sinθ) ψ / 2)
+    # = cos(ψ / 2) + i * sin(ψ / 2) * (σ_x cosθ + σ_y sinθ)
+    # = [cos(ψ / 2)                    i * sin(ψ / 2) (cosθ - i sinθ)
+    #    i * sin(ψ / 2) (cosθ + i sinθ)  cos(ψ / 2)]
+    sinθ, cosθ = sincos(θ)
+    sinψ_2, cosψ_2 = sincos(ψ / 2)
+    return [cosψ_2 sinψ_2 * (im * cosθ + sinθ)
+            sinψ_2 * (im * cosθ - sinθ) cosψ_2]
+end
