ae628ee5b5643d8478f9558a824d80c79d8e2b01
Yichao Yu <yyc1992@gmail.com>
2023-03-14 19:23:05 -0400

    [lib/MSSim] Add absolute tolerance to the integral function to avoid hanging when the integrand is almost zero

--- lib/MSSim/src/pure_numeric.jl
+++ lib/MSSim/src/pure_numeric.jl
@@ -10,20 +10,20 @@ using QuadGK
 
 function displacement(t0, t1, Ω, θ)
     f(t) = Ω(t) * cis(θ(t))
-    res, err = quadgk(f, t0, t1)
+    res, err = quadgk(f, t0, t1, atol=1e-8)
     return res
 end
 
 function cumulative_displacement(t0, t1, Ω, θ)
     f(t) = displacement(t0, t, Ω, θ)
-    res, err = quadgk(f, t0, t1)
+    res, err = quadgk(f, t0, t1, atol=1e-8)
     return res
 end
 
 # Twice the enclosed area
 function enclosed_area_complex(t0, t1, Ω, θ)
     f(t) = Ω(t) * cis(θ(t)) * displacement(t0, t, Ω, t->-θ(t))
-    res, err = quadgk(f, t0, t1)
+    res, err = quadgk(f, t0, t1, atol=1e-8)
     return res
 end
 
@@ -31,7 +31,7 @@ end
 function enclosed_area(t0, t1, Ω, θ)
     # Only getting the interesting part of the integral.
     f(t) = Ω(t) * imag(cis(θ(t)) * displacement(t0, t, Ω, t->-θ(t)))
-    res, err = quadgk(f, t0, t1)
+    res, err = quadgk(f, t0, t1, atol=1e-8)
     return res
 end
 
