248698c8df8c16d87e937eff6a7c68b386f2c30d
Yichao Yu <yyc1992@gmail.com>
2023-03-13 21:23:24 -0400

    [lib/MSSim] Better expressions for zero detuning

--- lib/MSSim/src/sym_linear.jl
+++ lib/MSSim/src/sym_linear.jl
@@ -5,20 +5,33 @@ module SymLinear
 # Integral for pulses that are piecewise linear in both amplitude and phase.
 
 module SegInt
-# Integral for each segments
 
+import ...Utils
+
+# Integral for each segments
 function displacement(τ, Ω, Ω′, φ, δ)
     phase0 = exp(im * φ)
-    phase1 = exp(im * δ * τ)
-    return phase0 / δ^2 * ((-im * Ω * δ + Ω′) * (phase1 - 1)
-                            - im * Ω′ * δ * τ * phase1)
+    d = δ * τ
+    o = Ω * τ
+    o′ = Ω′ * τ^2
+    s, c = sincos(d)
+    S = Utils.sinc(d, s, c)
+    C = Utils.cosc(d, s, c)
+    C1 = Utils.cos_f1(d, s, c)
+    return phase0 * ((im * (o * d) - o′) * C1 + (o + o′) * S - im * (o′ * C))
 end
 
 function cumulative_displacement(τ, Ω, Ω′, φ, δ)
     phase0 = exp(im * φ)
-    phase1 = exp(im * δ * τ)
-    return -phase0 / δ^2 * ((Ω + 2 * im * Ω′ / δ) * (phase1 - 1)
-                            + (Ω′ + Ω′ * phase1 - im * Ω * δ) * τ)
+    d = δ * τ
+    o = Ω * τ
+    o′ = Ω′ * τ^2
+    s, c = sincos(d)
+    C1 = Utils.cos_f1(d, s, c)
+    S1 = Utils.sin_f1(d, s, c)
+    C2 = Utils.cos_f2(d, s, c)
+    S2 = Utils.sin_f2(d, s, c)
+    return phase0 * τ * (o * C1 + o′ * S2 + im * (o * S1 - o′ * C2))
 end
 
 end
