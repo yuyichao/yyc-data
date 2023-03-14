a5ad13d65fb62fab5859d1b76015b135ba98a6a0
Yichao Yu <yyc1992@gmail.com>
2023-03-13 22:41:55 -0400

    [lib/MSSim] split out unique code

--- lib/MSSim/src/sym_linear.jl
+++ lib/MSSim/src/sym_linear.jl
@@ -9,57 +9,71 @@ module SegInt
 import ...Utils
 
 # Integral for each segments
-function displacement(τ, Ω, Ω′, φ, δ)
-    phase0 = exp(im * φ)
-    d = δ * τ
-    o = Ω * τ
-    o′ = Ω′ * τ^2
-    s, c = sincos(d)
+function displacement_kernel(o, o′, d, s, c)
     S = Utils.sinc(d, s, c)
     C = Utils.cosc(d, s, c)
     C1 = Utils.cos_f1(d, s, c)
-    return phase0 * ((im * (o * d) - o′) * C1 + (o + o′) * S - im * (o′ * C))
+    return complex((o + o′) * S - o′ * C1, o * d * C1 - o′ * C)
 end
 
-function cumulative_displacement(τ, Ω, Ω′, φ, δ)
-    phase0 = exp(im * φ)
-    d = δ * τ
-    o = Ω * τ
-    o′ = Ω′ * τ^2
-    s, c = sincos(d)
+function cumulative_displacement_kernel(o, o′, d, s, c)
     C1 = Utils.cos_f1(d, s, c)
     S1 = Utils.sin_f1(d, s, c)
     C2 = Utils.cos_f2(d, s, c)
     S2 = Utils.sin_f2(d, s, c)
-    return phase0 * τ * (o * C1 + o′ * S2 + im * (o * S1 - o′ * C2))
+    return complex(o * C1 + o′ * S2, o * S1 - o′ * C2)
+end
+
+function enclosed_area_complex_kernel(o, o′, d, s, c)
+    a1 = o^2 + o * o′
+    a2 = o′^2
+    C1 = Utils.cos_f1(d, s, c)
+    S1 = Utils.sin_f1(d, s, c)
+    C3 = Utils.cos_f3(d, s, c)
+    S3 = Utils.sin_f3(d, s, c)
+    return complex(a1 * C1 + a2 * C3, a1 * S1 + a2 * S3)
+end
+
+function enclosed_area_kernel(o, o′, d, s, c)
+    a1 = o^2 + o * o′
+    a2 = o′^2
+    C1 = Utils.cos_f1(d, s, c)
+    C3 = Utils.cos_f3(d, s, c)
+    return a1 * C1 + a2 * C3
+end
+
+function displacement(τ, Ω, Ω′, φ, δ)
+    phase0 = cis(φ)
+    d = δ * τ
+    o = Ω * τ
+    o′ = Ω′ * τ^2
+    s, c = sincos(d)
+    return phase0 * displacement_kernel(o, o′, d, s, c)
+end
+
+function cumulative_displacement(τ, Ω, Ω′, φ, δ)
+    phase0 = cis(φ)
+    d = δ * τ
+    o = Ω * τ
+    o′ = Ω′ * τ^2
+    s, c = sincos(d)
+    return phase0 * τ * cumulative_displacement_kernel(o, o′, d, s, c)
 end
 
 function enclosed_area_complex(τ, Ω, Ω′, φ, δ)
     d = δ * τ
     o = Ω * τ
     o′ = Ω′ * τ^2
-
-    a1 = o^2 + o * o′
-    a2 = o′^2
-
     s, c = sincos(d)
-    C1 = Utils.cos_f1(d, s, c)
-    S1 = Utils.sin_f1(d, s, c)
-    C3 = Utils.cos_f3(d, s, c)
-    S3 = Utils.sin_f3(d, s, c)
-    return a1 * C1 + a2 * C3 + im * (a1 * S1 + a2 * S3)
+    return enclosed_area_complex_kernel(o, o′, d, s, c)
 end
 
 function enclosed_area(τ, Ω, Ω′, φ, δ)
     d = δ * τ
     o = Ω * τ
     o′ = Ω′ * τ^2
-    a1 = o^2 + o * o′
-    a2 = o′^2
     s, c = sincos(d)
-    C1 = Utils.cos_f1(d, s, c)
-    C3 = Utils.cos_f3(d, s, c)
-    return a1 * C1 + a2 * C3
+    return enclosed_area_kernel(o, o′, d, s, c)
 end
 
 end
