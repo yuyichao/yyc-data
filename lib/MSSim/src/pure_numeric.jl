23285690c468f9a7ab62f4a980f831b5d20ca36f
Yichao Yu <yyc1992@gmail.com>
2023-03-27 17:03:21 -0400

    [lib/MSSim] Add tolerance to numerical integration methods

--- lib/MSSim/src/pure_numeric.jl
+++ lib/MSSim/src/pure_numeric.jl
@@ -8,30 +8,31 @@ using QuadGK
 # Done completely numerically. This is likely the slowest
 # but should be useful to test other faster implementations.
 
-function displacement(t0, t1, Ω, θ)
+function displacement(t0, t1, Ω, θ; atol=1e-8, rtol=1e-8)
     f(t) = Ω(t) * cis(θ(t))
-    res, err = quadgk(f, t0, t1, atol=1e-8)
+    res, err = quadgk(f, t0, t1; atol=atol, rtol=rtol)
     return res
 end
 
-function cumulative_displacement(t0, t1, Ω, θ)
-    f(t) = displacement(t0, t, Ω, θ)
-    res, err = quadgk(f, t0, t1, atol=1e-8)
-    return res
-end
-
-# Twice the enclosed area
-function enclosed_area_complex(t0, t1, Ω, θ)
-    f(t) = Ω(t) * cis(θ(t)) * displacement(t0, t, Ω, t->-θ(t))
-    res, err = quadgk(f, t0, t1, atol=1e-8)
+function cumulative_displacement(t0, t1, Ω, θ; atol=1e-8, rtol=1e-8)
+    f(t) = displacement(t0, t, Ω, θ; atol=atol, rtol=rtol)
+    res, err = quadgk(f, t0, t1; atol=atol, rtol=rtol)
     return res
 end
 
 # Twice the enclosed area
-function enclosed_area(t0, t1, Ω, θ)
+function enclosed_area_complex(t0, t1, Ω, θ; atol=1e-8, rtol=1e-8)
+    f(t) = Ω(t) * cis(θ(t)) * displacement(t0, t, Ω, t->-θ(t); atol=atol, rtol=rtol)
+    res, err = quadgk(f, t0, t1; atol=atol, rtol=rtol)
+    return res
+end
+
+# Twice the enclosed area
+function enclosed_area(t0, t1, Ω, θ; atol=1e-8, rtol=1e-8)
     # Only getting the interesting part of the integral.
-    f(t) = Ω(t) * imag(cis(θ(t)) * displacement(t0, t, Ω, t->-θ(t)))
-    res, err = quadgk(f, t0, t1, atol=1e-8)
+    f(t) = Ω(t) * imag(cis(θ(t)) * displacement(t0, t, Ω, t->-θ(t),
+                                                  atol=atol, rtol=rtol))
+    res, err = quadgk(f, t0, t1; atol=atol, rtol=rtol)
     return res
 end
 
