331e9fc80990cec4ca27f2fd5dfc1e5dc2954cad
Yichao Yu <yyc1992@gmail.com>
2023-04-23 19:22:52 -0400

    [euriqa/calculation/tweezer_coupling] angular momentum operators

---
+++ euriqa/calculations/tweezer_coupling/utils.jl
@@ -0,0 +1,38 @@
+#!/usr/bin/julia
+
+function add_J₊!(M::AbstractMatrix{T}, scale=1) where T
+    (s1, s2) = size(M)
+    @assert s1 == s2
+    J = (s1 - 1) / T(2)
+    for i in 1:(s1 - 1)
+        m = i - J - 1
+        M[i + 1, i] += sqrt((J - m) * (J + m + 1)) * scale
+    end
+    return M
+end
+
+function add_J₋!(M::AbstractMatrix{T}, scale=1) where T
+    (s1, s2) = size(M)
+    @assert s1 == s2
+    J = (s1 - 1) / T(2)
+    for i in 2:s1
+        m = i - J - 1
+        M[i - 1, i] += sqrt((J + m) * (J - m + 1)) * scale
+    end
+    return M
+end
+
+# J+ = Jx + i Jy
+# J- = Jx - i Jy
+
+# (J+ + J-) / 2 = Jx
+function add_Jx!(M::AbstractMatrix{T}, scale=1) where T
+    add_J₊!(M, scale / T(2))
+    add_J₋!(M, scale / T(2))
+end
+
+# (J+ - J-) / 2i = Jy
+function add_Jx!(M::AbstractMatrix{T}, scale=1) where T
+    add_J₊!(M, scale / T(2im))
+    add_J₋!(M, -scale / T(2im))
+end
