cf9056c2790e771fd1205025c6217055a875dad7
Yichao Yu <yyc1992@gmail.com>
2023-04-26 10:05:37 -0400

    [euriqa/calculation/tweezer_coupling] vector and tensor shift

--- euriqa/calculations/tweezer_coupling/utils.jl
+++ euriqa/calculations/tweezer_coupling/utils.jl
@@ -1,5 +1,7 @@
 #!/usr/bin/julia
 
+using LinearAlgebra
+
 function add_J₊!(M::AbstractMatrix{T}, scale=1) where T
     (s1, s2) = size(M)
     @assert s1 == s2
@@ -46,4 +48,40 @@ end
 function add_Jy!(M::AbstractMatrix{T}, scale=1) where T
     add_J₊!(M, scale / T(2im))
     add_J₋!(M, -scale / T(2im))
+end
+
+function add_vector_shift!(M::AbstractMatrix{T}, α, u) where T
+    (s1, s2) = size(M)
+    @assert s1 == s2
+    J = (s1 - 1) / T(2)
+    V = imag(u × conj(u)) ./ 2 .* α ./ J
+    add_Jx!(M, V[1])
+    add_Jy!(M, V[2])
+    add_Jz!(M, V[3])
+    return M
+end
+
+function add_tensor_shift!(M::AbstractMatrix{T}, α, u) where T
+    (s1, s2) = size(M)
+    @assert s1 == s2
+    J = (s1 - 1) / T(2)
+
+    M1 = similar(M)
+    M2 = similar(M)
+    M1 .= zero(T)
+    M2 .= zero(T)
+
+    add_Jx!(M1, u[1])
+    add_Jy!(M1, u[2])
+    add_Jz!(M1, u[3])
+
+    add_Jx!(M2, conj(u[1]))
+    add_Jy!(M2, conj(u[2]))
+    add_Jz!(M2, conj(u[3]))
+
+    M′ = 3 * (M1 * M2 + M2 * M1) - 2 * J * (J + 1) * I
+
+    M .+= M′ .* α ./ (2 * J * (2 * J - 1))
+
+    return M
 end
