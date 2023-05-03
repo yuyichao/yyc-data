e8edb6c5ede96c74718bf30d8da3a01b20c16c63
Yichao Yu <yyc1992@gmail.com>
2023-05-03 18:28:18 -0400

    [euriqa/calculation/master_equation] Python version with similar interface to mesolve

---
+++ euriqa/calculations/master_equation/const_mesolve.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+
+from qutip import *
+import numpy as np
+from scipy.linalg import expm
+from qutip.ui.progressbar import BaseProgressBar, TextProgressBar
+
+def _add_H(M, N, H):
+    if isinstance(H, (QobjEvo,)):
+        raise Exception("Time dependent Hamiltonian not supported.")
+    H = Qobj(H)
+    for i in range(N):
+        for j in range(N):
+            mi = i * N + j
+            for k in range(N):
+                M[mi, k * N + j] += H[i, k] / 1j
+                M[mi, i * N + k] -= H[k, j] / 1j
+
+def _add_C(M, N, C):
+    if isinstance(C, (QobjEvo,)):
+        raise Exception("Time dependent collapse term not supported.")
+    C = Qobj(C)
+    Ch = C.conj().trans()
+    L = Ch * C
+    for i in range(N):
+        for j in range(N):
+            mi = i * N + j
+            for k in range(N):
+                for l in range(N):
+                    M[mi, k * N + l] += C[i, k] * Ch[l, j]
+                M[mi, k * N + j] -= L[i, k] / 2
+                M[mi, i * N + k] -= L[k, j] / 2
+
+def _construct_matrix(H, c_ops):
+    hshape = H.shape
+    assert len(hshape) == 2
+    assert hshape[0] == hshape[1]
+    N = hshape[0]
+    M = np.zeros((N**2, N**2), dtype=complex) # Use dense matrix for now
+    _add_H(M, N, H)
+    for C in c_ops:
+        _add_C(M, N, C)
+    return M, N
+
+def const_mesolve(H, rho0, tlist, c_ops=None, e_ops=None, progress_bar=None):
+    if c_ops is None:
+        c_ops = []
+    elif isinstance(c_ops, (Qobj,)):
+        c_ops = [c_ops]
+    elif isinstance(c_ops, (QobjEvo,)):
+        raise Exception("Time dependent collapse term not supported.")
+
+    if e_ops is None:
+        e_ops = []
+    if isinstance(e_ops, Qobj):
+        e_ops = [e_ops]
+
+    if isinstance(e_ops, dict):
+        e_ops_dict = e_ops
+        e_ops = [e for e in e_ops.values()]
+    else:
+        e_ops_dict = None
+
+    if progress_bar is None:
+        progress_bar = BaseProgressBar()
+    if progress_bar is True:
+        progress_bar = TextProgressBar()
+
+    M, N = _construct_matrix(H, c_ops)
+
+    if isket(rho0):
+        rho0 = ket2dm(rho0)
+
+    n_tsteps = len(tlist)
+    output = solver.Result()
+    output.solver = "const_mesolve"
+    output.times = tlist
+    output.expect = []
+    output.states = []
+    output.num_collapse = len(c_ops)
+
+    if callable(e_ops):
+        n_expt_op = 0
+        expt_callback = True
+        output.num_expect = 1
+    elif isinstance(e_ops, list):
+        n_expt_op = len(e_ops)
+        expt_callback = False
+        output.num_expect = n_expt_op
+        for op in e_ops:
+            if not isinstance(op, Qobj) and callable(op):
+                output.expect.append(np.zeros(n_tsteps, dtype=complex))
+                continue
+            if op.dims != rho0.dims:
+                raise TypeError(f"e_ops dims ({op.dims}) are not "
+                                f"compatible with the state's "
+                                f"({rho0.dims})")
+            if op.isherm and rho0.isherm:
+                output.expect.append(np.zeros(n_tsteps))
+            else:
+                output.expect.append(np.zeros(n_tsteps, dtype=complex))
+    else:
+        raise TypeError("Expectation parameter must be a list or a function")
+
+    rhof0 = np.ndarray.flatten(np.array(rho0))
+
+    progress_bar.start(n_tsteps)
+    for t_idx, t in enumerate(tlist):
+        progress_bar.update(t_idx)
+        rhof = expm(t * M) @ rhof0
+        rho = Qobj(rhof.reshape(N, N))
+        output.states.append(rho)
+
+        if expt_callback:
+            # use callback method
+            output.expect.append(e_ops(t, rho))
+
+        for m in range(n_expt_op):
+            if not isinstance(e_ops[m], Qobj) and callable(e_ops[m]):
+                output.expect[m][t_idx] = e_ops[m](t, rho_t)
+                continue
+            output.expect[m][t_idx] = expect(e_ops[m], rho)
+
+    if e_ops_dict:
+        output.expect = {e: output.expect[n] for n, e in enumerate(e_ops_dict.keys())}
+
+    return output
