d4daae66972cd8d919d5a7d747a0dabcfb23a556
Yichao Yu <yyc1992@gmail.com>
2023-06-01 13:17:42 -0400

    [lib/NaCsCalc] test for clifford gates

---
+++ lib/NaCsCalc/test/clifford.jl
@@ -0,0 +1,58 @@
+#!/usr/bin/julia
+
+module TestClifford
+
+using Test
+
+using NaCsCalc
+
+const Clf = NaCsCalc.Clifford
+
+function test_gate_1q(gate, inputs, outputs, sign)
+    @test Clf.apply(gate, inputs..., false) === (outputs..., sign)
+    @test Clf.apply(gate, inputs..., true) === (outputs..., !sign)
+end
+
+@testset "X" begin
+    test_gate_1q(Clf.XGate(), (false, false), (false, false), false)
+    test_gate_1q(Clf.XGate(), (true, false), (true, false), false)
+    test_gate_1q(Clf.XGate(), (true, true), (true, true), true)
+    test_gate_1q(Clf.XGate(), (false, true), (false, true), true)
+end
+
+@testset "Y" begin
+    test_gate_1q(Clf.YGate(), (false, false), (false, false), false)
+    test_gate_1q(Clf.YGate(), (true, false), (true, false), true)
+    test_gate_1q(Clf.YGate(), (true, true), (true, true), false)
+    test_gate_1q(Clf.YGate(), (false, true), (false, true), true)
+end
+
+@testset "Z" begin
+    test_gate_1q(Clf.ZGate(), (false, false), (false, false), false)
+    test_gate_1q(Clf.ZGate(), (true, false), (true, false), true)
+    test_gate_1q(Clf.ZGate(), (true, true), (true, true), true)
+    test_gate_1q(Clf.ZGate(), (false, true), (false, true), false)
+end
+
+@testset "H" begin
+    test_gate_1q(Clf.HGate(), (false, false), (false, false), false)
+    test_gate_1q(Clf.HGate(), (true, false), (false, true), false)
+    test_gate_1q(Clf.HGate(), (true, true), (true, true), true)
+    test_gate_1q(Clf.HGate(), (false, true), (true, false), false)
+end
+
+@testset "S" begin
+    test_gate_1q(Clf.SGate(), (false, false), (false, false), false)
+    test_gate_1q(Clf.SGate(), (true, false), (true, true), false)
+    test_gate_1q(Clf.SGate(), (true, true), (true, false), true)
+    test_gate_1q(Clf.SGate(), (false, true), (false, true), false)
+end
+
+@testset "SX" begin
+    test_gate_1q(Clf.SXGate(), (false, false), (false, false), false)
+    test_gate_1q(Clf.SXGate(), (true, false), (true, false), false)
+    test_gate_1q(Clf.SXGate(), (true, true), (false, true), false)
+    test_gate_1q(Clf.SXGate(), (false, true), (true, true), true)
+end
+
+end
