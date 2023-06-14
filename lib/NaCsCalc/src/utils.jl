d98535d60513971bb9383dd8b1e554916a99c5d5
Yichao Yu <yyc1992@gmail.com>
2023-06-14 10:31:55 -0400

    [lib/NaCsCalc] 2 qubit depolarization error

--- lib/NaCsCalc/src/utils.jl
+++ lib/NaCsCalc/src/utils.jl
@@ -322,6 +322,108 @@ end
     return res_x, res_z
 end
 
+
+mutable struct Rand2QDepol{T<:Union{Bool,Base.BitInteger},R<:AbstractRNG,P<:Real}
+    state_x1::UInt128
+    state_z1::UInt128
+    state_x2::UInt128
+    state_z2::UInt128
+    ele_left::Int
+    err_sel_left::Int
+    err_sel_state::UInt64
+    # if p_L >= 0, it is the original probability
+    # if p_L < 0, it's 1 / log1p(-p) used for computing the amount to jump ahead.
+    const p_L::P
+    const rng::R
+
+    function Rand2QDepol{T}(r::R, p::P) where {T<:Union{Bool,Base.BitInteger},R<:AbstractRNG,P<:Real}
+        0 <= p <= 1 || throw(ArgumentError("probability $p not in [0,1]"))
+        if 0 < p <= 0.17 # empirical threshold for trivial O(n) algorithm to be better
+            p = 1 / log1p(-p) # L > 0
+        end
+        return new{T,R,P}(0, 0, 0, 0, 0, 0, 0, p, r)
+    end
+    Rand2QDepol{T}(p::Real) where T<:Union{Bool,Base.BitInteger} =
+        Rand2QDepol{T}(Random.default_rng(), p)
+end
+
+@inline function _rand_err_sel(rd::Rand2QDepol)
+    @inline if rd.err_sel_left == 0
+        rd.err_sel_state = rand(rd.rng, zero(UInt64):UInt64(UInt64(15)^14 - 1))
+        rd.err_sel_left = 14
+    end
+    rd.err_sel_state, res = divrem(rd.err_sel_state, 15)
+    rd.err_sel_left -= 1
+    return res % Int
+end
+
+function _new_state(rd::Rand2QDepol)
+    Te = UInt128
+    n = sizeof(Te) * 8
+    S1 = zero(Te)
+    S2 = zero(Te)
+    S3 = zero(Te)
+    S4 = zero(Te)
+    r = rd.rng
+    p = rd.p_L
+    @inline if p >= 0
+        if p == 0
+            return S1, S2, S3, S4
+        end
+        for i = 1:n
+            v = rand(r)
+            if v >= p
+                continue
+            end
+            v = unsafe_trunc(Int, floor(v * (15 / p))) + 1
+            S1 |= Te(v & 1) << (i - 1)
+            S2 |= Te((v >> 1) & 1) << (i - 1)
+            S3 |= Te((v >> 2) & 1) << (i - 1)
+            S4 |= Te(v >> 3) << (i - 1)
+        end
+    else
+        i = 0
+        while true
+            s = randexp(r) * -p
+            s >= n - i && return S1, S2, S3, S4 # compare before ceil to avoid overflow
+            i += unsafe_trunc(Int, ceil(s))
+            v = _rand_err_sel(rd) + 1
+            S1 |= Te(v & 1) << (i - 1)
+            S2 |= Te((v >> 1) & 1) << (i - 1)
+            S3 |= Te((v >> 2) & 1) << (i - 1)
+            S4 |= Te(v >> 3) << (i - 1)
+        end
+    end
+    return S1, S2, S3, S4
+end
+
+@inline function Random.rand(rd::Rand2QDepol{T}) where T
+    Te = UInt128
+    @inline if sizeof(T) == sizeof(Te)
+        x1, z1, x2, z2 = _new_state(rd)
+        return x1 % T, z1 % T, x2 % T, z2 % T
+    end
+    n = sizeof(Te) * 8
+    state_x1 = rd.state_x1
+    state_z1 = rd.state_z1
+    state_x2 = rd.state_x2
+    state_z2 = rd.state_z2
+    if rd.ele_left == 0
+        rd.ele_left = n ÷ _nbits(T)
+        state_x1, state_z1, state_x2, state_z2 = _new_state(rd)
+    end
+    rd.ele_left -= 1
+    res_x1 = state_x1 % T
+    res_z1 = state_z1 % T
+    res_x2 = state_x2 % T
+    res_z2 = state_z2 % T
+    rd.state_x1 = state_x1 >> _nbits(T)
+    rd.state_z1 = state_z1 >> _nbits(T)
+    rd.state_x2 = state_x2 >> _nbits(T)
+    rd.state_z2 = state_z2 >> _nbits(T)
+    return res_x1, res_z1, res_x2, res_z2
+end
+
 function __init__()
     interactive_str = get(ENV, "NACS_INTERACT", "true")
     if interactive_str == "false" || interactive_str == "0"
