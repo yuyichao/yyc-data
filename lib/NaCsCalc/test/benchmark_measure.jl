9cfd00a7fc857bf3ec8792f870933b58c35a53fe
Yichao Yu <yyc1992@gmail.com>
2023-07-09 19:22:43 -0400

    [lib/NaCsCalc] Benchmark numbers

--- lib/NaCsCalc/test/benchmark_measure.jl
+++ lib/NaCsCalc/test/benchmark_measure.jl
@@ -14,7 +14,8 @@ const Clf = NaCsCalc.Clifford
     return v1, v2
 end
 
-function test_size_rep(state, n, rep)
+function test_size_rep(state, rep)
+    n = state.n
     for i in 1:rep
         Clf.measure_xs!(state, rand_2bits(n))
         Clf.measure_zs!(state, rand_2bits(n))
@@ -23,30 +24,38 @@ end
 
 function test_size(::Type{SST}, n, rep) where SST
     state = SST(n)
-    @btime test_size_rep($state, $n, $rep)
+    @btime test_size_rep($state, $rep)
 end
 
-# 9.458 μs (4 allocations: 384 bytes)
+# 8.999 μs (0 allocations: 0 bytes)
+# 17.083 μs (0 allocations: 0 bytes)
 test_size(Clf.StabilizerState, 4, 100)
 test_size(Clf.InvStabilizerState, 4, 100)
-# 24.665 μs (4 allocations: 512 bytes)
+# 23.499 μs (0 allocations: 0 bytes)
+# 94.166 μs (0 allocations: 0 bytes)
 test_size(Clf.StabilizerState, 8, 200)
 test_size(Clf.InvStabilizerState, 8, 200)
-# 70.330 μs (4 allocations: 784 bytes)
+# 66.577 μs (0 allocations: 0 bytes)
+# 407.796 μs (0 allocations: 0 bytes)
 test_size(Clf.StabilizerState, 16, 400)
 test_size(Clf.InvStabilizerState, 16, 400)
-# 227.823 μs (4 allocations: 1.28 KiB)
+# 215.855 μs (0 allocations: 0 bytes)
+# 1.539 ms (0 allocations: 0 bytes)
 test_size(Clf.StabilizerState, 32, 800)
 test_size(Clf.InvStabilizerState, 32, 800)
-# 1.000 ms (4 allocations: 3.27 KiB)
+# 961.165 μs (0 allocations: 0 bytes)
+# 5.862 ms (0 allocations: 0 bytes)
 test_size(Clf.StabilizerState, 64, 1600)
 test_size(Clf.InvStabilizerState, 64, 1600)
-# 12.429 ms (6 allocations: 136.41 KiB)
+# 15.016 ms (0 allocations: 0 bytes)
+# 61.126 ms (0 allocations: 0 bytes)
 test_size(Clf.StabilizerState, 512, 1600)
 test_size(Clf.InvStabilizerState, 512, 1600)
-# 27.677 ms (6 allocations: 136.41 KiB)
+# 30.375 ms (0 allocations: 0 bytes)
+# 126.432 ms (0 allocations: 0 bytes)
 test_size(Clf.StabilizerState, 512, 3200)
 test_size(Clf.InvStabilizerState, 512, 3200)
-# 580.491 ms (7 allocations: 2.03 MiB)
+# 947.757 ms (0 allocations: 0 bytes)
+# 2.664 s (0 allocations: 0 bytes)
 test_size(Clf.StabilizerState, 2048, 6400)
 test_size(Clf.InvStabilizerState, 2048, 6400)
