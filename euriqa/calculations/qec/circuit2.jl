a5ab628a1f8180cf2f99a1a5ed36bf596b272204
Yichao Yu <yyc1992@gmail.com>
2023-08-01 18:29:57 -0400

    [euriqa/calculation/qec] generic circuit model: syndrome lot

---
+++ euriqa/calculations/qec/circuit2.jl
@@ -0,0 +1,81 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../../lib"))
+
+using NaCsCalc
+using NaCsCalc.Utils: RandSetBits, RandDepol, Rand2QDepol
+using NaCsPlot
+using PyPlot
+
+const Clf = NaCsCalc.Clifford
+
+struct ErrorBit
+    bit::Int
+    type::Int
+end
+
+function _compute_syndrome(errs::Vector{ErrorBit}, stab_x, stab_z)
+    res = false
+    for err in errs
+        if err.type == 1
+            res ⊻= stab_z[err.bit]
+        elseif err.type == 2
+            res ⊻= stab_x[err.bit] ⊻ stab_z[err.bit]
+        elseif err.type == 3
+            res ⊻= stab_x[err.bit]
+        end
+    end
+    return res
+end
+
+function _next_err_type!(errs)
+    for i in 1:length(errs)
+        err = errs[i]
+        if err.type >= 3
+            errs[i] = ErrorBit(err.bit, 1)
+            continue
+        end
+        errs[i] = ErrorBit(err.bit, err.type + 1)
+        return true
+    end
+    return false
+end
+
+function _next_bit!(errs, nq)
+    prev_bit = 0
+    nerr = length(errs)
+    for i in 1:nerr
+        err = errs[nerr - i + 1]
+        max_bit = nq + 1 - i
+        if err.bit < max_bit
+            bit_base = err.bit
+            for j in 1:i
+                errs[nerr - i + j] = ErrorBit(bit_base + j, 1)
+            end
+            return true
+        end
+    end
+    return false
+end
+
+function decoding_table(nq, t, stabs_x, stabs_z)
+    nstab = length(stabs_x)
+    lot = Dict{Vector{Bool},Vector{ErrorBit}}()
+    @assert length(stabs_z) == nstab
+    for nerr in 1:t
+        errs = [ErrorBit(i, 1) for i in 1:nerr]
+        while true
+            syndrome = [_compute_syndrome(errs, stabs_x[i], stabs_z[i])
+                        for i in 1:nstab]
+            if any(syndrome) && !(syndrome in keys(lot))
+                lot[syndrome] = copy(errs)
+            end
+            if !_next_err_type!(errs)
+                if !_next_bit!(errs, nq)
+                    break
+                end
+            end
+        end
+    end
+    return lot
+end
