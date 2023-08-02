c709ff52c0be564ae8fcea265cd9e1567311accf
Yichao Yu <yyc1992@gmail.com>
2023-08-02 15:39:50 -0400

    [euriqa/calculation/qec] generic circuit model: test raw measurement model

--- euriqa/calculations/qec/circuit2.jl
+++ euriqa/calculations/qec/circuit2.jl
@@ -267,3 +267,114 @@ function run(circ::RawStabMeasureCircuit{T}, nrep) where T
     end
     return err_stat
 end
+
+# const stabs_x = [[true, true, true, true, false, false, false],
+#                  [false, true, true, false, true, true, false],
+#                  [false, false, true, true, false, true, true],
+#                  [false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false]]
+# const stabs_z = [[false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false],
+#                  [true, true, true, true, false, false, false],
+#                  [false, true, true, false, true, true, false],
+#                  [false, false, true, true, false, true, true]]
+
+# const stabs_x = [[true, true, true, true, false, false, false],
+#                  [false, true, true, false, true, true, false],
+#                  [false, false, true, true, false, true, true],
+#                  [true, true, true, true, false, false, false],
+#                  [false, true, true, false, true, true, false],
+#                  [false, false, true, true, false, true, true]]
+# const stabs_z = [[false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false],
+#                  [true, true, true, true, false, false, false],
+#                  [false, true, true, false, true, true, false],
+#                  [false, false, true, true, false, true, true]]
+
+const stabs_x = [[true, true, true, true, false, false, false],
+                 [false, true, true, false, true, true, false],
+                 [false, false, true, true, false, true, true],
+                 [false, false, false, false, false, false, false],
+                 [false, false, false, false, false, false, false],
+                 [false, false, false, false, false, false, false]]
+const stabs_z = [[true, true, true, true, false, false, false],
+                 [false, true, true, false, true, true, false],
+                 [false, false, true, true, false, true, true],
+                 [true, true, true, true, false, false, false],
+                 [false, true, true, false, true, true, false],
+                 [false, false, true, true, false, true, true]]
+
+# const logics_x = [[false, false, false, false, true, true, true],
+#                   [false, false, false, false, false, false, false]]
+# const logics_z = [[false, false, false, false, false, false, false],
+#                   [false, false, false, false, true, true, true]]
+
+# const logics_x = [[false, false, false, false, true, true, true],
+#                   [false, false, false, false, true, true, true]]
+# const logics_z = [[false, false, false, false, false, false, false],
+#                   [false, false, false, false, true, true, true]]
+
+const logics_x = [[false, false, false, false, true, true, true],
+                  [false, false, false, false, false, false, false]]
+const logics_z = [[false, false, false, false, true, true, true],
+                  [false, false, false, false, true, true, true]]
+
+function calc_errors(ps, n)
+    eps = Vector{Float64}(undef, length(ps))
+    T = UInt128
+    for (i, p) in enumerate(ps)
+        init = UniformInit2{T}(7, p, 0.0)
+        # init = UniformInit{T}(p)
+        circ = RawStabMeasureCircuit{T}(stabs_x, stabs_z, logics_x, logics_z,
+                                        zeros(6), zeros(6),
+                                        zeros(7, 6), init)
+        # circ = RawStabMeasureCircuit{T}(stabs_x, stabs_z, logics_x, logics_z,
+        #                                 ones(6) .* 0.0001, ones(6) .* 0.0001,
+        #                                 ones(7, 6) .* 0.0001, init)
+        err_stat = run(circ, n)
+        eps[i] = err_stat.err / err_stat.tot
+    end
+    return eps
+end
+
+const ps = range(0.0001, 1, 200)
+const ps2 = exp10.(range(-5, 0, 1001))
+
+const prefix = joinpath(@__DIR__, "imgs/steane_circuit/")
+
+const eps2 = (
+    ps = ps2,
+    n0 = @time(calc_errors(ps2, 320 * 256 * 8)),
+)
+
+function plot_lines(eps)
+    plot(eps.ps, eps.n0, "C1")
+    plot(eps.ps, eps.ps, "C0--")
+end
+
+figure()
+plot_lines(eps2)
+grid()
+xlabel("Physical Error")
+ylabel("Logical Error")
+xlim([0, 0.15])
+ylim([0, 0.15])
+title("[[7, 1, 3]] Circuit")
+NaCsPlot.maybe_save("$(prefix)zoomin")
+
+figure()
+plot_lines(eps2)
+grid()
+xlabel("Physical Error")
+ylabel("Logical Error")
+xlim([1e-5, 0.15])
+ylim([1e-7, 0.11])
+xscale("log")
+yscale("log")
+title("[[7, 1, 3]] Circuit")
+NaCsPlot.maybe_save("$(prefix)log")
+
+NaCsPlot.maybe_show()
