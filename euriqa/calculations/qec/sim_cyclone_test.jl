196272d693dc15c9a9cb54b5aa88402fc6e964eb
Yichao Yu <yyc1992@gmail.com>
2023-08-03 21:33:53 -0400

    [euriqa/calculation/qec] test

---
+++ euriqa/calculations/qec/sim_cyclone_test.jl
@@ -0,0 +1,140 @@
+#!/usr/bin/julia
+
+include("sim_cyclone_lib.jl")
+
+# Steane
+const stabs_x = [[true, true, true, true, false, false, false],
+                 [false, true, true, false, true, true, false],
+                 [false, false, true, true, false, true, true],
+                 [false, false, false, false, false, false, false],
+                 [false, false, false, false, false, false, false],
+                 [false, false, false, false, false, false, false]]
+const stabs_z = [[false, false, false, false, false, false, false],
+                 [false, false, false, false, false, false, false],
+                 [false, false, false, false, false, false, false],
+                 [true, true, true, true, false, false, false],
+                 [false, true, true, false, true, true, false],
+                 [false, false, true, true, false, true, true]]
+
+# const stabs_x = [[true, true, true, true, false, false, false],
+#                  [false, true, true, false, true, true, false],
+#                  [false, false, true, true, false, true, true],
+#                  [true, true, true, true, false, false, false],
+#                  [false, true, true, false, true, true, false],
+#                  [false, false, true, true, false, true, true]]
+# const stabs_z = [[false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false],
+#                  [true, true, true, true, false, false, false],
+#                  [false, true, true, false, true, true, false],
+#                  [false, false, true, true, false, true, true]]
+
+# const stabs_x = [[true, true, true, true, false, false, false],
+#                  [false, true, true, false, true, true, false],
+#                  [false, false, true, true, false, true, true],
+#                  [false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false]]
+# const stabs_z = [[true, true, true, true, false, false, false],
+#                  [false, true, true, false, true, true, false],
+#                  [false, false, true, true, false, true, true],
+#                  [true, true, true, true, false, false, false],
+#                  [false, true, true, false, true, true, false],
+#                  [false, false, true, true, false, true, true]]
+
+const logics_x = [[false, false, false, false, true, true, true],
+                  [false, false, false, false, false, false, false]]
+const logics_z = [[false, false, false, false, false, false, false],
+                  [false, false, false, false, true, true, true]]
+
+# const logics_x = [[false, false, false, false, true, true, true],
+#                   [false, false, false, false, true, true, true]]
+# const logics_z = [[false, false, false, false, false, false, false],
+#                   [false, false, false, false, true, true, true]]
+
+# const logics_x = [[false, false, false, false, true, true, true],
+#                   [false, false, false, false, false, false, false]]
+# const logics_z = [[false, false, false, false, true, true, true],
+#                   [false, false, false, false, true, true, true]]
+
+# Shor
+# const stabs_x = [[true, true, false, false, false, false, false, false, false],
+#                  [false, true, true, false, false, false, false, false, false],
+#                  [false, false, false, true, true, false, false, false, false],
+#                  [false, false, false, false, true, true, false, false, false],
+#                  [false, false, false, false, false, false, true, true, false],
+#                  [false, false, false, false, false, false, false, true, true],
+#                  [false, false, false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false, false, false]]
+# const stabs_z = [[false, false, false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false, false, false],
+#                  [false, false, false, false, false, false, false, false, false],
+#                  [true, true, true, true, true, true, false, false, false],
+#                  [false, false, false, true, true, true, true, true, true]]
+
+# const logics_x = [[true, false, false, true, false, false, true, false, false],
+#                   [false, false, false, false, false, false, false, false, false]]
+# const logics_z = [[false, false, false, false, false, false, false, false, false],
+#                   [true, true, true, false, false, false, false, false, false]]
+
+
+function calc_errors(ps, n)
+    eps = Vector{Float64}(undef, length(ps))
+    T = UInt128
+    nstab = length(stabs_x)
+    nq = length(stabs_x[1])
+    for (i, p) in enumerate(ps)
+        init = UniformInit2{T}(nq, p, 0.0)
+        # init = UniformInit{T}(p)
+        # rngs = RNGs{T}(zeros(6), zeros(6 * 2), zeros(24))
+        rngs = RNGs{T}(ones(6) .* 0.0001, ones(6 * 2) .* 0.0001, ones(24) .* 0.0001)
+        # rngs = RNGs{T}(ones(6) .* p, ones(6 * 2) .* p, ones(24) .* p)
+        circ = RawStabMeasureCircuit{T}(stabs_x, stabs_z, logics_x, logics_z,
+                                        rngs, init)
+        err_stat = run(circ, n)
+        eps[i] = err_stat.err / err_stat.tot
+    end
+    return eps
+end
+
+const ps = range(0.0001, 1, 200)
+const ps2 = exp10.(range(-5, 0, 1001))
+
+const prefix = joinpath(@__DIR__, "imgs/steane_circuit/")
+
+const eps2 = (
+    ps = ps2,
+    n0 = @time(calc_errors(ps2, 320 * 256 * 8)),
+)
+
+function plot_lines(eps)
+    plot(eps.ps, eps.n0, "C1")
+    plot(eps.ps, eps.ps, "C0--")
+end
+
+figure()
+plot_lines(eps2)
+grid()
+xlabel("Physical Error")
+ylabel("Logical Error")
+xlim([0, 0.15])
+ylim([0, 0.15])
+title("[[7, 1, 3]] Circuit")
+NaCsPlot.maybe_save("$(prefix)zoomin")
+
+figure()
+plot_lines(eps2)
+grid()
+xlabel("Physical Error")
+ylabel("Logical Error")
+xlim([1e-5, 0.15])
+ylim([1e-7, 0.11])
+xscale("log")
+yscale("log")
+title("[[7, 1, 3]] Circuit")
+NaCsPlot.maybe_save("$(prefix)log")
+
+NaCsPlot.maybe_show()
