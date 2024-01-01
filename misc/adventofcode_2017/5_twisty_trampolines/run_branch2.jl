79f074554634d2ab42aae58d4d6bfe40e8aea725
Yichao Yu <yyc1992@gmail.com>
2024-01-01 01:30:56 -0500

    [adventofcode_2017] day 5.2

---
+++ misc/adventofcode_2017/5_twisty_trampolines/run_branch2.jl
@@ -0,0 +1,29 @@
+#!/usr/bin/julia
+
+function run_inst(insts, idx)
+    inst = insts[idx]
+    if inst >= 3
+        insts[idx] = inst - 1
+    else
+        insts[idx] = inst + 1
+    end
+    idx += inst
+    if idx < 1 || idx > length(insts)
+        return
+    end
+    return idx
+end
+
+function find_escape(file)
+    insts = [parse(Int, line) for line in eachline(file)]
+
+    idx = 1
+    c = 0
+    while idx !== nothing
+        c += 1
+        idx = run_inst(insts, idx)
+    end
+    return c
+end
+
+@show find_escape(ARGS[1])
