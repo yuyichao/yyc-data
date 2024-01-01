392fa98439b9b9340872a034b8392e4f7b9ca4f6
Yichao Yu <yyc1992@gmail.com>
2024-01-01 00:42:43 -0500

    [adventofcode_2022] day 5.2

---
+++ misc/adventofcode_2022/5_supply_stacks/top_crate2.jl
@@ -0,0 +1,48 @@
+#!/usr/bin/julia
+
+struct MoveInst
+    from::Int
+    to::Int
+    count::Int
+end
+
+function move_crates!(stacks, inst)
+    from = stacks[inst.from]
+    to = stacks[inst.to]
+
+    nfrom = length(from)
+
+    append!(to, @view(from[nfrom - inst.count + 1:nfrom]))
+    resize!(from, nfrom - inst.count)
+end
+
+function top_crate(file)
+    lines = eachline(file)
+    stacks = Vector{String}[]
+    for line in lines
+        if isempty(line)
+            break
+        end
+        for m in eachmatch(r"\[([A-Z])\]", line)
+            stacknum = (m.offset + 3) ÷ 4
+            name = m[1]
+            nstacks = length(stacks)
+            if nstacks < stacknum
+                resize!(stacks, stacknum)
+                for i in nstacks + 1:stacknum
+                    stacks[i] = String[]
+                end
+            end
+            pushfirst!(stacks[stacknum], name)
+        end
+    end
+
+    for line in lines
+        m = match(r"move (\d+) from (\d+) to (\d+)", line)
+        move_crates!(stacks, MoveInst(parse(Int, m[2]), parse(Int, m[3]),
+                                      parse(Int, m[1])))
+    end
+    return join([stack[end] for stack in stacks])
+end
+
+@show top_crate(ARGS[1])
