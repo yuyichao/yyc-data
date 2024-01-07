b0bf481dd8be45cab871b88e07de808da4e5ef76
Yichao Yu <yyc1992@gmail.com>
2024-01-07 17:43:09 -0500

    [adventofcode_2020] day 2.1

---
+++ misc/adventofcode_2020/2_password_philosophy/count_valid.jl
@@ -0,0 +1,18 @@
+#!/usr/bin/julia
+
+function check_password(pw, lo, hi, c0)
+    return lo <= count(==(c0), pw) <= hi
+end
+
+function count_valid(file)
+    c = 0
+    for line in eachline(file)
+        m = match(r"(\d+)-(\d+) (.): *(.+)", line)
+        if check_password(m[4], parse(Int, m[1]), parse(Int, m[2]), m[3][1])
+            c += 1
+        end
+    end
+    return c
+end
+
+@show count_valid(ARGS[1])
