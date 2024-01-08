53f3b69f3471500c57721580e09e0e8757d8c450
Yichao Yu <yyc1992@gmail.com>
2024-01-08 09:23:07 -0500

    [adventofcode_2015] day 4.2

---
+++ misc/adventofcode_2015/4_ideal_stocking_stuffer/find_hash2.jl
@@ -0,0 +1,16 @@
+#!/usr/bin/julia
+
+using MD5
+
+function find_hash(prefix)
+    suffix_num = 0
+    while true
+        h = md5(prefix * string(suffix_num))
+        if h[1] == 0 && h[2] == 0 && h[3] == 0
+            return suffix_num
+        end
+        suffix_num += 1
+    end
+end
+
+@show find_hash("yzbqklnj")
