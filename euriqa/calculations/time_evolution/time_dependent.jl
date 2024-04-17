79a0a116dd044394433da9d3540defe4b29f9c10
Yichao Yu <yyc1992@gmail.com>
2024-04-17 16:15:10 -0400

    [euriqa/calculation/time_evolution] Magnus expansion

--- euriqa/calculations/time_evolution/time_dependent.jl
+++ euriqa/calculations/time_evolution/time_dependent.jl
@@ -1,6 +1,7 @@
 #!/usr/bin/julia
 
 using QuantumOptics
+using LinearAlgebra
 
 struct LabBasisData{B,K,O} <: Function
     Ω0_2::Float64
@@ -106,6 +107,98 @@ function evolve(calc::RotBasisCalc, npoints=1001)
         p = cis(φ_2)
         data[1, i] *= p
         data[2, i] *= conj(p)
+    end
+    return ts, data
+end
+
+struct MagnusCalc
+    A0::Matrix{ComplexF64}
+    A1::Matrix{ComplexF64}
+    A01::Matrix{ComplexF64}
+    A011::Matrix{ComplexF64}
+    A0100::Matrix{ComplexF64}
+    A0101::Matrix{ComplexF64}
+    A0110::Matrix{ComplexF64}
+    A0111::Matrix{ComplexF64}
+
+    Abuff::Matrix{ComplexF64}
+    ψbuff1::Vector{ComplexF64}
+    ψbuff2::Vector{ComplexF64}
+    ψ0::Vector{ComplexF64}
+
+    δ0_2::Float64
+    dδ_4::Float64
+    tlen::Float64
+
+    function MagnusCalc(Ω0, Ω1, δ0, δ1, tlen)
+        H0 = [δ0 / 2 Ω0 / 2
+              Ω0 / 2 -δ0 / 2]
+        H1 = [(δ1 - δ0) / 2 (Ω1 - Ω0) / 2
+              (Ω1 - Ω0) / 2 -(δ1 - δ0) / 2]
+        H1 ./= tlen
+
+        A0 = H0 .* -im
+        A1 = H1 .* -im
+
+        A01 = A0 * A1 - A1 * A0
+
+        A010 = A01 * A0 - A0 * A01
+        A011 = A01 * A1 - A1 * A01
+
+        A0100 = A010 * A0 - A0 * A010
+        A0101 = A010 * A1 - A1 * A010
+        A0110 = A011 * A0 - A0 * A011
+        A0111 = A011 * A1 - A1 * A011
+
+        ψ0 = [0, 1]
+
+        return new(A0, A1, A01, A011, A0100, A0101, A0110, A0111,
+                   zeros(ComplexF64, 2, 2), zeros(ComplexF64, 2), zeros(ComplexF64, 2),
+                   ψ0, δ0 / 2, (δ1 - δ0) / tlen / 4, tlen)
+    end
+end
+
+function evolve(calc::MagnusCalc, npoints=1001)
+    tlen = calc.tlen
+
+    ts = [range(0, tlen, npoints);]
+    data = Matrix{ComplexF64}(undef, 2, npoints)
+
+    A0 = calc.A0
+    A1 = calc.A1
+    A01 = calc.A01
+    A011 = calc.A011
+    A0100 = calc.A0100
+    A0101 = calc.A0101
+    A0110 = calc.A0110
+    A0111 = calc.A0111
+
+    Abuff = calc.Abuff
+    ψbuff1 = calc.ψbuff1
+    ψbuff2 = calc.ψbuff2
+
+    data[:, 1] .= calc.ψ0
+    ψbuff1 .= calc.ψ0
+    δ0_2 = calc.δ0_2
+    dδ_4 = calc.dδ_4
+
+    @inbounds for i in 2:npoints
+        t1 = ts[i - 1]
+        t2 = ts[i]
+        dt = t2 - t1
+        Abuff .= (dt .* (A0 .+ (t1 + t2) / 2 .* A1) .- dt^3 / 12 .* A01 .+
+            dt^5 / 240 .* (A011 .+ 1 / 3 .* (
+                A0100 .+ (t1 + 5 * t2) / 6 .* A0101 .+
+                    (5 * t1 + t2) / 6 .* A0110 .+
+                    (t1^2 + 5 * t1 * t2 + t2^2) / 7 .* A0111)))
+        LinearAlgebra.mul!(ψbuff2, LinearAlgebra.exp!(Abuff), ψbuff1)
+
+        φ_2 = t2 * muladd(dδ_4, t2, δ0_2)
+        p = cis(φ_2)
+        data[1, i] = ψbuff2[1] * p
+        data[2, i] = ψbuff2[2] * conj(p)
+
+        ψbuff1, ψbuff2 = ψbuff2, ψbuff1
     end
     return ts, data
 end
