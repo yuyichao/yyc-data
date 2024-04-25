1ff895cb575b323edc5919de3936de015080698c
Yichao Yu <yyc1992@gmail.com>
2024-04-25 13:44:05 -0400

    [euriqa/calculation/dc_filter] filter with the correct ordering and playing with stage 1 and 2 resistor value

--- euriqa/calculations/dc_filter/test.jl
+++ euriqa/calculations/dc_filter/test.jl
@@ -29,7 +29,7 @@ function filter_435(ω, zload)
     return 1 / vi[1]
 end
 
-function filter(ω, zload)
+function filter_inv_order(ω, zload)
     vi = (1, 0)
     # load
     vi = add_parallel(vi, zload)
@@ -51,6 +51,32 @@ function filter(ω, zload)
     # stage 1
     vi = add_parallel(vi, zc(33e-9, ω))
     vi = add_series(vi, zr(150))
+
+    return 1 / vi[1]
+end
+
+function filter(ω, zload, stage12r=10)
+    vi = (1, 0)
+    # load
+    vi = add_parallel(vi, zload)
+
+    # stage 3
+    vi = add_parallel(vi, zc(33e-9, ω))
+    vi = add_series(vi, zr(150)) # 32kHz
+
+    # stage 2.2
+    vi = add_parallel(vi, zc(100e-6, ω))
+    vi = add_series(vi, zl(10e-6, ω) + zr(stage12r)) # L:5kHz, R:160Hz
+    # stage 2.1
+    vi = add_parallel(vi, zc(100e-6, ω))
+    vi = add_series(vi, zl(10e-6, ω) + zr(stage12r))
+
+    # stage 1.2
+    vi = add_parallel(vi, zc(200e-6, ω))
+    vi = add_series(vi, zl(100e-6, ω) + zr(stage12r)) # L:1.1kHz, R:80Hz
+    # stage 1.1
+    vi = add_parallel(vi, zc(200e-6, ω))
+    vi = add_series(vi, zl(100e-6, ω) + zr(stage12r))
 
     return 1 / vi[1]
 end
