3a4ef462e525fe3b4969eab534c054f9010b88f3
Yichao Yu <yyc1992@gmail.com>
2024-06-06 12:19:45 -0500

    [misc/vapor_pressure] Fix Ag data

--- misc/vapor_pressure/vapor_data.jl
+++ misc/vapor_pressure/vapor_data.jl
@@ -40,7 +40,7 @@ const InData = VaporPressureData(Tmelt=429.7485 - 273.15, As=5.991, Bs=-12548,
                                  Al=5.374, Bl=-12276)
 const TlData = VaporPressureData(Tmelt=577 - 273.15, As=5.971, Bs=-9447,
                                  Al=5.259, Bl=-9037)
-const AgData = VaporPressureData(Tmelt=1234.93 - 273.15, As=9.127, Bs=-14999, Cs=0.7845,
+const AgData = VaporPressureData(Tmelt=1234.93 - 273.15, As=9.127, Bs=-14999, Cs=-0.7845,
                                  Al=5.752, Bl=-13827)
 const YData = VaporPressureData(Tmelt=1799 - 273.15, As=9.735, Bs=-22306, Cs=-0.8705,
                                 Al=5.795, Bl=-20341)
