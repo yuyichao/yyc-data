c91ac3bf24946df5e2cc28e16780efa983cd284b
Yichao Yu <yyc1992@gmail.com>
2024-11-04 10:13:45 -0600

    [euriqa/calculation/time_evolution] finishing up Yb171 ion model

--- euriqa/calculations/time_evolution/Yb171ion.jl
+++ euriqa/calculations/time_evolution/Yb171ion.jl
@@ -70,14 +70,90 @@ function get_hf_hamiltonian(gJ, Fl, hfl, hfh)
     return Operator(b, op_data)
 end
 
+function dipole_branch(q, dJ, dJ′, dI, dF, dF′, dmF, dmF′)
+    return ((-1)^(dF′ + (dmF + dJ + dI) ÷ 2) *
+        sqrt((dJ′ + 1) * (dF + 1) * (dF′ + 1)) *
+        f6j(dJ, dJ′, 2, dF′, dF, dI) * f3j(dF′, 2, dF, dmF′, 2 * q, dmF))
+end
+
+function fill_J!(op, Γ, q, dJ, dJ′, dI, idxFl, idxFl′)
+    sqrtΓ = sqrt(Γ)
+    dFs = abs(dJ - dI):2:(dJ + dI)
+    dF′s = abs(dJ′ - dI):2:(dJ′ + dI)
+    for (i, dF) in enumerate(dFs)
+        idxF = idxFl + i - 1
+        basisF = SpinBasis(dF//2)
+        for (i′, dF′) in enumerate(dF′s)
+            idxF′ = idxFl′ + i′ - 1
+            basisF′ = SpinBasis(dF′//2)
+            subop = Operator(basisF, basisF′,
+                             [sqrtΓ * dipole_branch(q, dJ, dJ′, dI, dF, dF′, dmF, dmF′)
+                              for dmF in -dF:2:dF, dmF′ in -dF′:2:dF′])
+            setblock!(op, subop, idxF, idxF′)
+        end
+    end
+    return op
+end
+
+function fill_dipole!(op⁺, op⁻, q, dJ, dJ′, dI, idxFl, idxFl′)
+    dFs = abs(dJ - dI):2:(dJ + dI)
+    dF′s = abs(dJ′ - dI):2:(dJ′ + dI)
+    for (i, dF) in enumerate(dFs)
+        idxF = idxFl + i - 1
+        basisF = SpinBasis(dF//2)
+        for (i′, dF′) in enumerate(dF′s)
+            idxF′ = idxFl′ + i′ - 1
+            basisF′ = SpinBasis(dF′//2)
+            subop = Operator(basisF, basisF′,
+                             [dipole_branch(q, dJ, dJ′, dI, dF, dF′, dmF, dmF′)
+                              for dmF in -dF:2:dF, dmF′ in -dF′:2:dF′])
+            setblock!(op⁻, subop, idxF, idxF′)
+            setblock!(op⁺, subop', idxF′, idxF)
+        end
+    end
+    return op⁺, op⁻
+end
+
 # State order,
 # S1/2 (F=0, F=1), P1/2 (F=0, F=1), D3/2 (F=1, F=2), [3/2]1/2 (F=0, F=1)
+#         1,   2,          3,   4,          5,   6,              7,   8
 
-struct Yb171SysData{Basis,O} <: Function
+struct Yb171Sys{Basis,O}
     basis::Basis
     op::O
-    h0::H
-    function Yb171SysData(B)
+    h0::O
+
+    J::Vector{O}
+    Jdagger::Vector{O}
+
+    dP_Sσ⁺⁺::O
+    dP_Sσ⁺⁻::O
+    dP_Sσ⁻⁺::O
+    dP_Sσ⁻⁻::O
+    dP_Sπ⁺::O
+    dP_Sπ⁻::O
+
+    dP_Dσ⁺⁺::O
+    dP_Dσ⁺⁻::O
+    dP_Dσ⁻⁺::O
+    dP_Dσ⁻⁻::O
+    dP_Dπ⁺::O
+    dP_Dπ⁻::O
+
+    dB_Sσ⁺⁺::O
+    dB_Sσ⁺⁻::O
+    dB_Sσ⁻⁺::O
+    dB_Sσ⁻⁻::O
+    dB_Sπ⁺::O
+    dB_Sπ⁻::O
+
+    dB_Dσ⁺⁺::O
+    dB_Dσ⁺⁻::O
+    dB_Dσ⁻⁺::O
+    dB_Dσ⁻⁻::O
+    dB_Dπ⁺::O
+    dB_Dπ⁻::O
+    function Yb171Sys(B)
         h_S1 = get_hf_hamiltonian(g_S1_2 * μB * B, 0, -HF_S1_2, 0)
         h_P1 = get_hf_hamiltonian(g_P1_2 * μB * B, 0, 0, HF_P1_2)
         h_D3 = get_hf_hamiltonian(g_D3_2 * μB * B, 1, 0, HF_D3_2)
@@ -85,146 +161,84 @@ struct Yb171SysData{Basis,O} <: Function
 
         h0 = h_S1 ⊕ h_P1 ⊕ h_D3 ⊕ h_B
 
-        # Γσ⁺
-        # Γσ⁻
-        # Γπ
+        basis = h0.basis_l
+
+        J = [fill_J!(zero(h0), ΓP_S, 1, 1, 1, 1, 1, 3),
+             fill_J!(zero(h0), ΓP_S, -1, 1, 1, 1, 1, 3),
+             fill_J!(zero(h0), ΓP_S, 0, 1, 1, 1, 1, 3),
+             fill_J!(zero(h0), ΓP_D, 1, 3, 1, 1, 5, 3),
+             fill_J!(zero(h0), ΓP_D, -1, 3, 1, 1, 5, 3),
+             fill_J!(zero(h0), ΓP_D, 0, 3, 1, 1, 5, 3),
+             fill_J!(zero(h0), ΓB_S, 1, 1, 1, 1, 1, 7),
+             fill_J!(zero(h0), ΓB_S, -1, 1, 1, 1, 1, 7),
+             fill_J!(zero(h0), ΓB_S, 0, 1, 1, 1, 1, 7),
+             fill_J!(zero(h0), ΓB_D, 1, 3, 1, 1, 5, 7),
+             fill_J!(zero(h0), ΓB_D, -1, 3, 1, 1, 5, 7),
+             fill_J!(zero(h0), ΓB_D, 0, 3, 1, 1, 5, 7)]
+        Jdagger = dagger.(J)
+
+        dP_Sσ⁺⁺, dP_Sσ⁺⁻ = fill_dipole!(zero(h0), zero(h0), 1, 1, 1, 1, 1, 3)
+        dP_Sσ⁻⁺, dP_Sσ⁻⁻ = fill_dipole!(zero(h0), zero(h0), -1, 1, 1, 1, 1, 3)
+        dP_Sπ⁺, dP_Sπ⁻ = fill_dipole!(zero(h0), zero(h0), 0, 1, 1, 1, 1, 3)
+
+        dP_Dσ⁺⁺, dP_Dσ⁺⁻ = fill_dipole!(zero(h0), zero(h0), 1, 3, 1, 1, 5, 3)
+        dP_Dσ⁻⁺, dP_Dσ⁻⁻ = fill_dipole!(zero(h0), zero(h0), -1, 3, 1, 1, 5, 3)
+        dP_Dπ⁺, dP_Dπ⁻ = fill_dipole!(zero(h0), zero(h0), 0, 3, 1, 1, 5, 3)
+
+        dB_Sσ⁺⁺, dB_Sσ⁺⁻ = fill_dipole!(zero(h0), zero(h0), 1, 1, 1, 1, 1, 7)
+        dB_Sσ⁻⁺, dB_Sσ⁻⁻ = fill_dipole!(zero(h0), zero(h0), -1, 1, 1, 1, 1, 7)
+        dB_Sπ⁺, dB_Sπ⁻ = fill_dipole!(zero(h0), zero(h0), 0, 1, 1, 1, 1, 7)
+
+        dB_Dσ⁺⁺, dB_Dσ⁺⁻ = fill_dipole!(zero(h0), zero(h0), 1, 3, 1, 1, 5, 7)
+        dB_Dσ⁻⁺, dB_Dσ⁻⁻ = fill_dipole!(zero(h0), zero(h0), -1, 3, 1, 1, 5, 7)
+        dB_Dπ⁺, dB_Dπ⁻ = fill_dipole!(zero(h0), zero(h0), 0, 3, 1, 1, 5, 7)
+
+        return new{typeof(basis),typeof(h0)}(
+            basis, zero(h0), h0, J, Jdagger,
+            dP_Sσ⁺⁺, dP_Sσ⁺⁻, dP_Sσ⁻⁺, dP_Sσ⁻⁻, dP_Sπ⁺, dP_Sπ⁻,
+            dP_Dσ⁺⁺, dP_Dσ⁺⁻, dP_Dσ⁻⁺, dP_Dσ⁻⁻, dP_Dπ⁺, dP_Dπ⁻,
+            dB_Sσ⁺⁺, dB_Sσ⁺⁻, dB_Sσ⁻⁺, dB_Sσ⁻⁻, dB_Sπ⁺, dB_Sπ⁻,
+            dB_Dσ⁺⁺, dB_Dσ⁺⁻, dB_Dσ⁻⁺, dB_Dσ⁻⁻, dB_Dπ⁺, dB_Dπ⁻
+        )
     end
 end
 
-struct LabBasisData{B,K,O} <: Function
-    Ω10_2::Float64
-    dΩ1_2::Float64
-    Ω20_2::Float64
-    dΩ2_2::Float64
-    δ10::Float64
-    dδ1_2::Float64
-    δ20::Float64
-    dδ2_2::Float64
-    tlen::Float64
-
-    basis::B
-    ψ0::K
-    op::O
-    function LabBasisData(Ω10, Ω11, Ω20, Ω21, δ10, δ11, δ20, δ21, tlen)
-        basis = GenericBasis(3)
-        op_data = zeros(ComplexF64, 3, 3)
-        op = Operator(basis, op_data)
-        ψ0 = basisstate(basis, 1)
-        Ω10_2 = Ω10 / 2
-        Ω20_2 = Ω20 / 2
-        dΩ1_2 = (Ω11 - Ω10) / tlen / 2
-        dΩ2_2 = (Ω21 - Ω20) / tlen / 2
-        dδ1_2 = (δ11 - δ10) / tlen / 2
-        dδ2_2 = (δ21 - δ20) / tlen / 2
-        return new{typeof(basis),typeof(ψ0),typeof(op)}(Ω10_2, dΩ1_2, Ω20_2, dΩ2_2,
-                                                         δ10, dδ1_2, δ20, dδ2_2,
-                                                         tlen, basis, ψ0, op)
+function get_ψ(sys::Yb171Sys, name::Symbol, F, mF)
+    ψ = Ket(sys.basis)
+    @assert -F <= mF <= F
+    if name === :S
+        @assert 0 <= F <= 1
+        ψ.data[F * 2 + mF + 1] = 1
+    elseif name === :P
+        @assert 0 <= F <= 1
+        ψ.data[F * 2 + mF + 5] = 1
+    elseif name === :D
+        @assert 1 <= F <= 2
+        ψ.data[F * 4 + mF + 6] = 1
+    elseif name === :B
+        @assert 0 <= F <= 1
+        ψ.data[F * 2 + mF + 17] = 1
+    else
+        @assert false
     end
+    return ψ
 end
 
-function (data::LabBasisData)(t, ψ)
-    op_data = data.op.data
-    # δi = δi0 + (δi1 - δi0) * t / tlen
-    # φi = ∫δidt = δi0 * t + (δi1 - δi0) * t^2 / 2 / tlen
-    φ1 = t * muladd(data.dδ1_2, t, data.δ10)
-    φ2 = t * muladd(data.dδ2_2, t, data.δ20)
-    Ω1_2 = muladd(data.dΩ1_2, t, data.Ω10_2)
-    Ω2_2 = muladd(data.dΩ2_2, t, data.Ω20_2)
-    M1 = Ω1_2 * cis(φ1)
-    M2 = Ω2_2 * cis(φ2)
-    op_data[1, 2] = M1
-    op_data[2, 1] = conj(M1)
-    op_data[2, 3] = M2
-    op_data[3, 2] = conj(M2)
-    return data.op
+function get_ρ(sys::Yb171Sys, name::Symbol, F, mF)
+    ψ = get_ψ(sys, name, F, mF)
+    return ψ ⊗ ψ'
 end
 
-struct LabBasisCalc{LD}
-    data::LD
-    function LabBasisCalc(args...)
-        ld = LabBasisData(args...)
-        return new{typeof(ld)}(ld)
+function evolve(update!, sys::Yb171Sys, ρ0, tlen, npoints=1001; kws...)
+    cb = let update! = update!, sys = sys
+        function (t, rho)
+            update!(sys, t)
+            return (sys.op, sys.J, sys.Jdagger)
+        end
     end
+    return timeevolution.master_dynamic(range(0, tlen, npoints), ρ0, cb; kws...)
 end
 
-function evolve(calc::LabBasisCalc, npoints=1001; kws...)
-    calc_data = calc.data
-    ts, ψs = timeevolution.schroedinger_dynamic(range(0, calc_data.tlen, npoints),
-                                                 calc_data.ψ0, calc_data; kws...)
-    return ts, [ψ.data[i] for i in 1:3, ψ in ψs]
-end
-
-struct RotBasisData{B,K,O} <: Function
-    Ω10_2::Float64
-    dΩ1_2::Float64
-    Ω20_2::Float64
-    dΩ2_2::Float64
-    δ10::Float64
-    dδ1_2::Float64
-    δ20::Float64
-    dδ2_2::Float64
-
-    tlen::Float64
-    basis::B
-    ψ0::K
-    op::O
-    function RotBasisData(Ω10, Ω11, Ω20, Ω21, δ10, δ11, δ20, δ21, tlen)
-        basis = GenericBasis(3)
-        op_data = zeros(ComplexF64, 3, 3)
-        op = Operator(basis, op_data)
-        ψ0 = basisstate(basis, 1)
-        Ω10_2 = Ω10 / 2
-        Ω20_2 = Ω20 / 2
-        dΩ1_2 = (Ω11 - Ω10) / tlen / 2
-        dΩ2_2 = (Ω21 - Ω20) / tlen / 2
-        dδ1_2 = (δ11 - δ10) / tlen / 2
-        dδ2_2 = (δ21 - δ20) / tlen / 2
-        return new{typeof(basis),typeof(ψ0),typeof(op)}(Ω10_2, dΩ1_2, Ω20_2, dΩ2_2,
-                                                         δ10, dδ1_2, δ20, dδ2_2,
-                                                         tlen, basis, ψ0, op)
-    end
-end
-
-function (data::RotBasisData)(t, ψ)
-    op_data = data.op.data
-    δ1 = muladd(data.dδ1_2 * 2, t, data.δ10)
-    δ2 = muladd(data.dδ2_2 * 2, t, data.δ20)
-    Ω1_2 = muladd(data.dΩ1_2, t, data.Ω10_2)
-    Ω2_2 = muladd(data.dΩ2_2, t, data.Ω20_2)
-    op_data[1, 1] = δ1
-    op_data[3, 3] = -δ2
-    op_data[1, 2] = Ω1_2
-    op_data[2, 1] = Ω1_2
-    op_data[2, 3] = Ω2_2
-    op_data[3, 2] = Ω2_2
-    return data.op
-end
-
-struct RotBasisCalc{LD}
-    data::LD
-    function RotBasisCalc(args...)
-        ld = RotBasisData(args...)
-        return new{typeof(ld)}(ld)
-    end
-end
-
-function evolve(calc::RotBasisCalc, npoints=1001; kws...)
-    calc_data = calc.data
-    ts, ψs = timeevolution.schroedinger_dynamic(range(0, calc_data.tlen, npoints),
-                                                 calc_data.ψ0, calc_data; kws...)
-    data = [ψ.data[i] for i in 1:3, ψ in ψs]
-    δ10 = calc_data.δ10
-    δ20 = calc_data.δ20
-    dδ1_2 = calc_data.dδ1_2
-    dδ2_2 = calc_data.dδ2_2
-    tlen = calc_data.tlen
-    for (i, t) in enumerate(ts)
-        φ1 = t * muladd(dδ1_2, t, δ10)
-        φ2 = t * muladd(dδ2_2, t, δ20)
-        p1 = cis(φ1)
-        p2 = cis(φ2)
-        data[1, i] *= p1
-        data[3, i] *= conj(p2)
-    end
-    return ts, data
+function no_drive!(sys::Yb171Sys, t)
+    sys.op .= sys.h0
 end
