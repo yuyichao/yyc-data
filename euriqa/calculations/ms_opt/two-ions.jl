f2b55e1409b6482d702c8caa8d427ad166e545a7
Yichao Yu <yyc1992@gmail.com>
2025-07-04 20:47:13 -0400

    [euriqa/calculations/ms_opt] more single mode test

--- euriqa/calculations/ms_opt/two-ions.jl
+++ euriqa/calculations/ms_opt/two-ions.jl
@@ -28,7 +28,9 @@ nseg = 30
 buf = SL.ComputeBuffer{nseg,Float64}(Val(SS.ValueMask(true, true, true, true, true, true)),
                                    Val(SS.ValueMask(true, true, true, true, true, true)));
 kern = SL.Kernel(buf, Val(SL.ParamGradMask(true, true, true, true, true)));
-args = Opts.gen_args(model, nseg, freq=Opts.FreqSpec(true))
+args = Opts.gen_args(model, nseg,
+                     freq=Opts.FreqSpec(true, sym=false),
+                     amp=Opts.AmpSpec(sym=false, mid_order=8, end_order=2))
 Opts.register_kernel_funcs(model, kern)
 
 modes = Opts.Modes()
@@ -42,23 +44,25 @@ areaδ = Opts.total_areaδ(model, args, modes)
 all_areaδ = Opts.all_areaδ(model, args, modes)
 
 tracker = Opts.VarTracker()
-push!(tracker, args.τ, 1, 10)
-push!(tracker, args.Ωs.poly[1], 0.1, 1)
+push!(tracker, args.τ, 1, 2)
+for Ω in args.Ωs.poly
+    push!(tracker, Ω, 0.05, 0.5)
+end
 for ω in args.ωs
     push!(tracker, ω, 1.0, 4.0)
 end
-obj = @NLexpression(model, (dis + disδ + 1e-15) / abs(area)^2)
+obj = @NLexpression(model, (dis + disδ + 1e-15) / (1 + area^2))
 @NLobjective(model, Min, obj)
 
 best_obj = 1.0
 best_params = nothing
-@time for _ in 1:1000
+@time for _ in 1:10000
     global best_obj, best_params
     Opts.init_vars(tracker)
     JuMP.optimize!(model)
     if value(obj) < best_obj
-        @show value(dis), value(disδ), value(cdis), value(area), value(areaδ), value(all_areaδ)
         best_obj = value(obj)
+        @show best_obj, value(dis), value(disδ), value(cdis), value(area), value(areaδ), value(all_areaδ)
         best_params = values.(all_variables(model))
     end
 end
