02ced03ea60a75c1d3a9fea1ec27309c8f58fee1
Yichao Yu <yyc1992@gmail.com>
2025-07-06 09:47:06 -0400

    [euriqa/calculations/ms_opt] 5 ion arobust

---
+++ euriqa/calculations/ms_opt/multi-ions.jl
@@ -0,0 +1,70 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../../lib"))
+
+using MSSim
+using JuMP
+using BenchmarkTools
+
+const Opts = MSSim.Optimizers
+const SS = MSSim.SegSeq
+const SL = MSSim.SymLinear
+
+using NLopt
+const model = Model(NLopt.Optimizer)
+set_optimizer_attribute(model, "algorithm", :LD_LBFGS)
+# set_optimizer_attribute(model, "algorithm", :LD_SLSQP)
+
+nseg = 40
+
+# buf = SL.ComputeBuffer{nseg,Float64}(Val(SS.ValueMask(true, true, true, false, true, false)),
+#                                    Val(SS.ValueMask(true, true, true, false, true, false)))
+# buf = SL.ComputeBuffer{nseg,Float64}(Val(SS.ValueMask(true, true, true, false, true, true)),
+#                                      Val(SS.ValueMask(true, true, true, false, true, true)))
+buf = SL.ComputeBuffer{nseg,Float64}(Val(SS.ValueMask(true, true, true, true, true, true)),
+                                     Val(SS.ValueMask(true, true, true, true, true, true)))
+kern = SL.Kernel(buf, Val(SL.ParamGradMask(true, true, true, true, true)));
+args = Opts.gen_args(model, nseg, freq=Opts.FreqSpec(true))
+Opts.register_kernel_funcs(model, kern)
+
+modes = Opts.Modes()
+for i in 1:5
+    push!(modes, (2.1 + 0.1 * i) * 2π, (-1)^i)
+end
+
+dis = Opts.total_dis(model, args, modes)
+cdis = Opts.total_cumdis(model, args, modes)
+area = Opts.total_area(model, args, modes)
+disδ = Opts.total_disδ(model, args, modes)
+areaδ = Opts.total_areaδ(model, args, modes)
+all_areaδ = Opts.all_areaδ(model, args, modes)
+
+tracker = Opts.VarTracker()
+push!(tracker, args.τ, 1, 6)
+for Ω in args.Ωs.poly
+    fix(Ω, 0.5)
+    # push!(tracker, Ω, 0.1, 0.5)
+end
+for ω in args.ωs
+    push!(tracker, ω, 2π * 2.0, 2π * 3.0)
+end
+obj = @NLexpression(model, (dis + disδ + areaδ^2 + 1e-10) / area^2)
+# obj = @NLexpression(model, (10 * dis + disδ + 1e-10) / (1 + abs(area)))
+# obj = @NLexpression(model, (cdis + 1e-10) / (1 + abs(area)))
+# obj = @NLexpression(model, cdis + 1e-10)
+# obj = @NLexpression(model, dis + disδ)
+@NLobjective(model, Min, obj)
+
+best_obj = 100.0
+best_params = nothing
+@time for i in 1:1000
+    global best_obj, best_params
+    Opts.init_vars(tracker)
+    @time JuMP.optimize!(model)
+    if value(obj) < best_obj
+        best_obj = value(obj)
+        @show best_obj, value(dis), value(disδ), value(area), value(areaδ)
+        best_params = value.(all_variables(model))
+        @show best_params
+    end
+end
