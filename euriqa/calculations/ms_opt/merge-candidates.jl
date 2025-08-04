4658af9c4fe2f6ed33c5240592fac60991218f13
Yichao Yu <yyc1992@gmail.com>
2025-08-04 10:10:10 -0400

    [euriqa/calculations/ms_opt] make sure the block size is consistent

--- euriqa/calculations/ms_opt/merge-candidates.jl
+++ euriqa/calculations/ms_opt/merge-candidates.jl
@@ -22,4 +22,4 @@ for d in ARGS[2:end]
 end
 append!(candidates, tail_candidates)
 println("Total $(length(candidates))")
-save_candidates(prefix, candidates, meta)
+save_candidates(prefix, candidates, meta, block_size=block_size)
