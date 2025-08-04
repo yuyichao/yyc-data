c03379114dae8ad1bbe33d638780d4c6db5661ca
Yichao Yu <yyc1992@gmail.com>
2025-08-04 10:02:11 -0400

    [euriqa/calculations/ms_opt] Script to merge candidates

---
+++ euriqa/calculations/ms_opt/merge-candidates.jl
@@ -0,0 +1,25 @@
+#!/usr/bin/julia
+
+include("opt-all-pair.jl")
+
+const prefix = ARGS[1]
+const block_size = 2000
+meta, candidates = load_candidates_dir(dirname(prefix))
+tail_candidates = Candidate[]
+function cut_tail!(candidates, tail_candidates)
+    n = length(candidates)
+    nround = n ÷ block_size * block_size
+    append!(tail_candidates, @view(candidates[nround + 1:end]))
+    resize!(candidates, nround)
+    return
+end
+cut_tail!(candidates, tail_candidates)
+println("Load original: $(length(candidates)) [tail: $(length(tail_candidates))]")
+for d in ARGS[2:end]
+    load_candidates_dir(d, meta=meta, candidates=candidates)
+    cut_tail!(candidates, tail_candidates)
+    println("Loaded $d: $(length(candidates)) [tail: $(length(tail_candidates))]")
+end
+append!(candidates, tail_candidates)
+println("Total $(length(candidates))")
+save_candidates(prefix, candidates, meta)
