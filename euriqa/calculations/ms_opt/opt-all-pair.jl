d5047651f2a3ee5892746173290ae1d76501ae55
Yichao Yu <yyc1992@gmail.com>
2025-09-11 13:58:32 -0400

    Use ThreadObjectPool from GoldGates

--- euriqa/calculations/ms_opt/opt-all-pair.jl
+++ euriqa/calculations/ms_opt/opt-all-pair.jl
@@ -11,69 +11,7 @@ using NLopt
 using Base.Threads
 using Printf
 
-mutable struct ObjSlot{T}
-    @atomic value::Union{T,Nothing}
-end
-
-mutable struct ThreadObjectPool{T,CB}
-    const lock::ReentrantLock
-    const cb::CB
-    # The array is used for lock-less fast path and needs to be accessed atomically
-    # The array itself will never by mutated (only the ObjSlot{} objects in it will)
-    # So access of the array member/size does not need to be atomic.
-    # Access of the Atomic variables stored in the array should all be atomic exchanges
-    # so that we never have duplicated/missing references to any objects.
-    @atomic array::Vector{ObjSlot{T}}
-    const extra::Vector{T} # Protected by lock
-    function ThreadObjectPool(cb::CB) where CB
-        obj = cb()
-        T = typeof(obj)
-        array = [ObjSlot{T}(i == 1 ? obj : nothing) for i in 1:Threads.maxthreadid()]
-        return new{T,CB}(ReentrantLock(), cb, array, T[])
-    end
-end
-
-function Base.get(pool::ThreadObjectPool{T}) where T
-    array = @atomic :acquire pool.array
-    id = Threads.threadid()
-    if id <= length(array)
-        obj = @atomicswap(:acquire_release, @inbounds(array[id]).value = nothing)
-        if obj !== nothing
-            return obj::T
-        end
-    end
-    @lock pool.lock begin
-        # Reload, in case someone else changed it
-        # No atomicity needed since the thread that may have changed it must
-        # have done so with the lock held.
-        array = pool.array
-        oldlen = length(array)
-        if id > oldlen
-            array = [i <= oldlen ? @inbounds(array[i]) : ObjSlot{T}(nothing)
-                     for i in 1:Threads.maxthreadid()]
-            @atomic :release pool.array = array
-        end
-        if !isempty(pool.extra)
-            return pop!(pool.extra)
-        end
-        return pool.cb()::T
-    end
-end
-
-function Base.put!(pool::ThreadObjectPool{T}, obj::T) where T
-    array = @atomic :acquire pool.array
-    id = Threads.threadid()
-    if id <= length(array)
-        obj = @atomicswap(:acquire_release, @inbounds(array[id]).value = obj)
-        if obj === nothing
-            return
-        end
-    end
-    @lock pool.lock begin
-        push!(pool.extra, obj)
-    end
-    return
-end
+using GoldGates.ThreadUtils: ThreadObjectPool
 
 function set_mode_weight!(weights, ηs, bij, ion1, ion2)
     nions = length(ηs)
