d7699ccc1e3b9c0ea6239d2a4427bc03a8ae2b08
Yichao Yu <yyc1992@gmail.com>
2025-12-19 19:14:24 -0500

    [euriqa/calculations/sparse_benchmark] Tweak test cases

--- euriqa/calculations/sparse_benchmarks/sparse.jl
+++ euriqa/calculations/sparse_benchmarks/sparse.jl
@@ -79,16 +79,59 @@ function spmul3(C::StridedMatrix, X::DenseMatrixUnion, A::SparseMatrixCSCUnion2,
     C
 end
 
+function spmul4(C::StridedMatrix, X::DenseMatrixUnion, A::SparseMatrixCSCUnion2, α::Number, β::Number)
+    mX, nX = size(X)
+    rv = rowvals(A)
+    nzv = nonzeros(A)
+    β_one = isone(β)
+    β_zero = iszero(β)
+    @inbounds for col in axes(A,2)
+        filled = β_one
+        C_col = @view(C[:, col])
+        for k in nzrange(A, col)
+            Aiα = nzv[k] * α
+            rvk = rv[k]
+            X_col = @view(X[:, rvk])
+            if filled
+                @simd for multivec_row in axes(X,1)
+                    C_col[multivec_row] = muladd(X_col[multivec_row], Aiα,
+                                                 C_col[multivec_row])
+                end
+            elseif β_zero
+                @simd for multivec_row in axes(X,1)
+                    C_col[multivec_row] = X_col[multivec_row] * Aiα
+                end
+            else
+                @simd for multivec_row in axes(X,1)
+                    C_col[multivec_row] = muladd(X_col[multivec_row], Aiα,
+                                                 C_col[multivec_row] * β)
+                end
+            end
+            filled = true
+        end
+        if !filled
+            if β_zero
+                fill!(C_col, zero(eltype(C)))
+            else
+                rmul!(C_col, β)
+            end
+        end
+    end
+    C
+end
+
 function bench_sparse(C, X, A)
     println("  false")
     @btime spmul1($C, $X, $A, true, false)
     @btime spmul2($C, $X, $A, true, false)
     @btime spmul3($C, $X, $A, true, false)
+    @btime spmul4($C, $X, $A, true, false)
 
     println("  true")
     @btime spmul1($C, $X, $A, true, true)
     @btime spmul2($C, $X, $A, true, true)
     @btime spmul3($C, $X, $A, true, true)
+    @btime spmul4($C, $X, $A, true, true)
 
     return
 end
@@ -103,28 +146,41 @@ function bench_sparse_type(ElType, sz)
     println(" 1%")
     bench_sparse(zeros(ElType, sz, sz), zeros(ElType, sz, sz),
                  sprand(ElType, sz, sz, 0.01))
+    println(" diag")
+    bench_sparse(zeros(ElType, sz, sz), zeros(ElType, sz, sz),
+                 sparse(1:sz, 1:sz, ones(ElType, sz)))
 end
 
+println("ComplexF64 10x10")
+bench_sparse_type(ComplexF64, 10)
 println("ComplexF64 100x100")
 bench_sparse_type(ComplexF64, 100)
 println("ComplexF64 1000x1000")
 bench_sparse_type(ComplexF64, 1000)
 
+println("Float64 10x10")
+bench_sparse_type(Float64, 10)
 println("Float64 100x100")
 bench_sparse_type(Float64, 100)
 println("Float64 1000x1000")
 bench_sparse_type(Float64, 1000)
 
+println("BigFloat 10x10")
+bench_sparse_type(BigFloat, 10)
 println("BigFloat 100x100")
 bench_sparse_type(BigFloat, 100)
-println("BigFloat 1000x1000")
-bench_sparse_type(BigFloat, 1000)
+# println("BigFloat 1000x1000")
+# bench_sparse_type(BigFloat, 1000)
 
+println("Int64 10x10")
+bench_sparse_type(Int64, 10)
 println("Int64 100x100")
 bench_sparse_type(Int64, 100)
 println("Int64 1000x1000")
 bench_sparse_type(Int64, 1000)
 
+println("Int32 10x10")
+bench_sparse_type(Int32, 10)
 println("Int32 100x100")
 bench_sparse_type(Int32, 100)
 println("Int32 1000x1000")
