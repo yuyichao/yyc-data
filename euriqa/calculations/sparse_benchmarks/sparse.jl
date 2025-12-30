d404ef9e2daf3f63df3897bf4642bf4d0e1b3caf
Yichao Yu <yyc1992@gmail.com>
2025-12-29 19:11:59 -0600

    [euriqa/calculations/sparse_benchmark] Update sparse benchmarks

--- euriqa/calculations/sparse_benchmarks/sparse.jl
+++ euriqa/calculations/sparse_benchmarks/sparse.jl
@@ -6,8 +6,11 @@ using SparseArrays: DenseMatrixUnion, SparseMatrixCSCUnion2
 
 using BenchmarkTools
 
-function spmul1(C::StridedMatrix, X::DenseMatrixUnion, A::SparseMatrixCSCUnion2, α::Number, β::Number)
-    mX, nX = size(X)
+@inline _fast_mul(a, b) = a * b
+@inline _fast_mul(a::Union{ComplexF16,ComplexF32,ComplexF64},
+                  b::Union{ComplexF16,ComplexF32,ComplexF64}) = muladd(a, b, false)
+
+function spmul_orig(C::StridedMatrix, X::DenseMatrixUnion, A::SparseMatrixCSCUnion2, α::Number, β::Number)
     rv = rowvals(A)
     nzv = nonzeros(A)
     β != one(β) && LinearAlgebra._rmul_or_fill!(C, β)
@@ -21,27 +24,41 @@ function spmul1(C::StridedMatrix, X::DenseMatrixUnion, A::SparseMatrixCSCUnion2,
     C
 end
 
-function spmul2(C::StridedMatrix, X::DenseMatrixUnion, A::SparseMatrixCSCUnion2, α::Number, β::Number)
-    mX, nX = size(X)
+function spmul_muladd(C::StridedMatrix, X::DenseMatrixUnion, A::SparseMatrixCSCUnion2, α::Number, β::Number)
+    rv = rowvals(A)
+    nzv = nonzeros(A)
+    β != one(β) && LinearAlgebra._rmul_or_fill!(C, β)
+    @inbounds for col in axes(A,2), k in nzrange(A, col)
+        Aiα = _fast_mul(nzv[k], α)
+        rvk = rv[k]
+        @simd for multivec_row in axes(X,1)
+            C[multivec_row, col] = muladd(X[multivec_row, rvk], Aiα,
+                                          C[multivec_row, col])
+        end
+    end
+    C
+end
+
+function spmul_view(C::StridedMatrix, X::DenseMatrixUnion, A::SparseMatrixCSCUnion2, α::Number, β::Number)
     rv = rowvals(A)
     nzv = nonzeros(A)
     β != one(β) && LinearAlgebra._rmul_or_fill!(C, β)
     @inbounds for col in axes(A,2)
         C_col = @view(C[:, col])
         for k in nzrange(A, col)
-            Aiα = nzv[k] * α
+            Aiα = _fast_mul(nzv[k], α)
             rvk = rv[k]
             X_col = @view(X[:, rvk])
             @simd for multivec_row in axes(X,1)
-                C_col[multivec_row] += X_col[multivec_row] * Aiα
+                C_col[multivec_row] = muladd(X_col[multivec_row], Aiα,
+                                             C_col[multivec_row])
             end
         end
     end
     C
 end
 
-function spmul3(C::StridedMatrix, X::DenseMatrixUnion, A::SparseMatrixCSCUnion2, α::Number, β::Number)
-    mX, nX = size(X)
+function spmul_split(C::StridedMatrix, X::DenseMatrixUnion, A::SparseMatrixCSCUnion2, α::Number, β::Number)
     rv = rowvals(A)
     nzv = nonzeros(A)
     β_one = isone(β)
@@ -50,20 +67,16 @@ function spmul3(C::StridedMatrix, X::DenseMatrixUnion, A::SparseMatrixCSCUnion2,
         filled = β_one
         C_col = @view(C[:, col])
         for k in nzrange(A, col)
-            Aiα = nzv[k] * α
+            Aiα = _fast_mul(nzv[k], α)
             rvk = rv[k]
             X_col = @view(X[:, rvk])
-            if filled
-                @simd for multivec_row in axes(X,1)
-                    C_col[multivec_row] += X_col[multivec_row] * Aiα
-                end
-            elseif β_zero
-                @simd for multivec_row in axes(X,1)
-                    C_col[multivec_row] = X_col[multivec_row] * Aiα
-                end
-            else
-                @simd for multivec_row in axes(X,1)
-                    C_col[multivec_row] = C_col[multivec_row] * β + X_col[multivec_row] * Aiα
+            @simd for multivec_row in axes(X,1)
+                C_col[multivec_row] = if filled
+                    muladd(X_col[multivec_row], Aiα, C_col[multivec_row])
+                elseif β_zero
+                    _fast_mul(X_col[multivec_row], Aiα)
+                else
+                    muladd(X_col[multivec_row], Aiα, _fast_mul(C_col[multivec_row], β))
                 end
             end
             filled = true
@@ -72,48 +85,9 @@ function spmul3(C::StridedMatrix, X::DenseMatrixUnion, A::SparseMatrixCSCUnion2,
             if β_zero
                 fill!(C_col, zero(eltype(C)))
             else
-                rmul!(C_col, β)
-            end
-        end
-    end
-    C
-end
-
-function spmul4(C::StridedMatrix, X::DenseMatrixUnion, A::SparseMatrixCSCUnion2, α::Number, β::Number)
-    mX, nX = size(X)
-    rv = rowvals(A)
-    nzv = nonzeros(A)
-    β_one = isone(β)
-    β_zero = iszero(β)
-    @inbounds for col in axes(A,2)
-        filled = β_one
-        C_col = @view(C[:, col])
-        for k in nzrange(A, col)
-            Aiα = nzv[k] * α
-            rvk = rv[k]
-            X_col = @view(X[:, rvk])
-            if filled
                 @simd for multivec_row in axes(X,1)
-                    C_col[multivec_row] = muladd(X_col[multivec_row], Aiα,
-                                                 C_col[multivec_row])
+                    C_col[multivec_row] = _fast_mul(C_col[multivec_row], β)
                 end
-            elseif β_zero
-                @simd for multivec_row in axes(X,1)
-                    C_col[multivec_row] = X_col[multivec_row] * Aiα
-                end
-            else
-                @simd for multivec_row in axes(X,1)
-                    C_col[multivec_row] = muladd(X_col[multivec_row], Aiα,
-                                                 C_col[multivec_row] * β)
-                end
-            end
-            filled = true
-        end
-        if !filled
-            if β_zero
-                fill!(C_col, zero(eltype(C)))
-            else
-                rmul!(C_col, β)
             end
         end
     end
@@ -122,16 +96,16 @@ end
 
 function bench_sparse(C, X, A)
     println("  false")
-    @btime spmul1($C, $X, $A, true, false)
-    @btime spmul2($C, $X, $A, true, false)
-    @btime spmul3($C, $X, $A, true, false)
-    @btime spmul4($C, $X, $A, true, false)
+    @btime spmul_orig($C, $X, $A, true, false)
+    @btime spmul_muladd($C, $X, $A, true, false)
+    @btime spmul_view($C, $X, $A, true, false)
+    @btime spmul_split($C, $X, $A, true, false)
 
     println("  true")
-    @btime spmul1($C, $X, $A, true, true)
-    @btime spmul2($C, $X, $A, true, true)
-    @btime spmul3($C, $X, $A, true, true)
-    @btime spmul4($C, $X, $A, true, true)
+    @btime spmul_orig($C, $X, $A, true, true)
+    @btime spmul_muladd($C, $X, $A, true, true)
+    @btime spmul_view($C, $X, $A, true, true)
+    @btime spmul_split($C, $X, $A, true, true)
 
     return
 end
@@ -155,33 +129,23 @@ println("ComplexF64 10x10")
 bench_sparse_type(ComplexF64, 10)
 println("ComplexF64 100x100")
 bench_sparse_type(ComplexF64, 100)
-println("ComplexF64 1000x1000")
-bench_sparse_type(ComplexF64, 1000)
 
 println("Float64 10x10")
 bench_sparse_type(Float64, 10)
 println("Float64 100x100")
 bench_sparse_type(Float64, 100)
-println("Float64 1000x1000")
-bench_sparse_type(Float64, 1000)
 
 println("BigFloat 10x10")
 bench_sparse_type(BigFloat, 10)
 println("BigFloat 100x100")
 bench_sparse_type(BigFloat, 100)
-# println("BigFloat 1000x1000")
-# bench_sparse_type(BigFloat, 1000)
 
 println("Int64 10x10")
 bench_sparse_type(Int64, 10)
 println("Int64 100x100")
 bench_sparse_type(Int64, 100)
-println("Int64 1000x1000")
-bench_sparse_type(Int64, 1000)
 
 println("Int32 10x10")
 bench_sparse_type(Int32, 10)
 println("Int32 100x100")
 bench_sparse_type(Int32, 100)
-println("Int32 1000x1000")
-bench_sparse_type(Int32, 1000)
