6e595e172ff87caa98fd90b5a1fee9b2cb82d924
Yichao Yu <yyc1992@gmail.com>
2025-12-30 20:27:07 -0600

    [euriqa/calculations/sparse_benchmark] save benchmark result

--- euriqa/calculations/sparse_benchmarks/bench_sparse.jl
+++ euriqa/calculations/sparse_benchmarks/bench_sparse.jl
@@ -3,60 +3,62 @@
 include("sparse.jl")
 
 using BenchmarkTools
+using YAML
 
-function bench_sparse(C, X, A)
-    println("  false")
-    @btime spmul_orig($C, $X, $A, true, false)
-    @btime spmul_view($C, $X, $A, true, false)
-    @btime spmul_muladd($C, $X, $A, true, false)
-    @btime spmul_split($C, $X, $A, true, false)
-
-    println("  true")
-    @btime spmul_orig($C, $X, $A, true, true)
-    @btime spmul_view($C, $X, $A, true, true)
-    @btime spmul_muladd($C, $X, $A, true, true)
-    @btime spmul_split($C, $X, $A, true, true)
-
-    return
+function bench_sparse_param(C, X, A, α, β)
+    return Dict{String,Any}("orig"=>@btimed(spmul_orig($C, $X, $A, $α, $β)),
+                            "view"=>@btimed(spmul_view($C, $X, $A, $α, $β)),
+                            "muladd"=>@btimed(spmul_muladd($C, $X, $A, $α, $β)),
+                            "split"=>@btimed(spmul_split($C, $X, $A, $α, $β)))
 end
 
-function bench_sparse_type(ElType, sz)
+function bench_sparse_matrix(C, X, A)
+    ET = eltype(C)
+    vs = Any[false, true, 0, 1, 2]
+    res = Dict{String,Any}[]
+    for α in vs
+        for β in vs
+            b = bench_sparse_param(f!, C, X, A, ET(α), ET(β))
+            b["α"] = α
+            b["β"] = β
+            push!(res, b)
+        end
+    end
+    return res
+end
+
+function bench_sparse_type_sz(ElType, sz)
+    C = zeros(ElType, sz, sz)
+    X = zeros(ElType, sz, sz)
+
     println(" 25%")
-    bench_sparse(zeros(ElType, sz, sz), zeros(ElType, sz, sz),
-                 sprand(ElType, sz, sz, 0.25))
+    b25 = bench_sparse_matrix(C, X, sprand(ElType, sz, sz, 0.25))
     println(" 5%")
-    bench_sparse(zeros(ElType, sz, sz), zeros(ElType, sz, sz),
-                 sprand(ElType, sz, sz, 0.05))
+    b5 = bench_sparse_matrix(C, X, sprand(ElType, sz, sz, 0.05))
     println(" 1%")
     A = sprand(ElType, sz, sz, 0.01)
     while nnz(A) == 0
         A = sprand(ElType, sz, sz, 0.01)
     end
-    bench_sparse(zeros(ElType, sz, sz), zeros(ElType, sz, sz), A)
+    b1 = bench_sparse_matrix(C, X, A)
     println(" 0%")
-    bench_sparse(zeros(ElType, sz, sz), zeros(ElType, sz, sz),
-                 spzeros(ElType, sz, sz))
+    b0 = bench_sparse_matrix(C, X, spzeros(ElType, sz, sz))
     println(" diag")
-    bench_sparse(zeros(ElType, sz, sz), zeros(ElType, sz, sz),
-                 sparse(1:sz, 1:sz, ones(ElType, sz)))
+    bdiag = bench_sparse_matrix(C, X, sparse(1:sz, 1:sz, ones(ElType, sz)))
+
+    return Dict("25"=>b25, "5"=>b5, "1"=>b1, "0"=>b0, "diag"=>bdiag)
 end
 
-println("ComplexF64 10x10")
-bench_sparse_type(ComplexF64, 10)
-println("ComplexF64 100x100")
-bench_sparse_type(ComplexF64, 100)
+function bench_sparse_type(ElType)
+    return Dict("10"=>bench_sparse_type_sz(ElType, 10),
+                "100"=>bench_sparse_type_sz(ElType, 100))
+end
 
-println("Float64 10x10")
-bench_sparse_type(Float64, 10)
-println("Float64 100x100")
-bench_sparse_type(Float64, 100)
+function bench_sparse()
+    return Dict("ComplexF64"=>bench_sparse_type(ComplexF64),
+                "Float64"=>bench_sparse_type(Float64),
+                "Int64"=>bench_sparse_type(Int64),
+                "BigFloat"=>bench_sparse_type(BigFloat))
+end
 
-println("BigFloat 10x10")
-bench_sparse_type(BigFloat, 10)
-println("BigFloat 100x100")
-bench_sparse_type(BigFloat, 100)
-
-println("Int64 10x10")
-bench_sparse_type(Int64, 10)
-println("Int64 100x100")
-bench_sparse_type(Int64, 100)
+YAML.write_file(ARGS[1], bench_sparse())
