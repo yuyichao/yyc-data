fb6d94fc26188460737e5772655b9b05a03f0407
Yichao Yu <yyc1992@gmail.com>
2026-05-22 12:36:04 -0400

    [euriqa/calculations/ms_opt] save candidates

--- euriqa/calculations/ms_opt/am_candidates.jl
+++ euriqa/calculations/ms_opt/am_candidates.jl
@@ -11,17 +11,111 @@ using Base.Threads
 
 using GoldGates
 
+import ProtoBuf as PB
+
 struct Candidate
     τ::Float64
     ω::Float64
     Ωs::Vector{Float64}
     props::Union{Nothing,Seq.SolutionProperties}
-    function Candidate(args, param, props)
-        @assert length(param.ωs) == 1
-        @assert length(args) == length(param.Ωs) + 2
-        return new(args[param.τ], args[param.ωs[1]],
-                   [args[Ω] for Ω in param.Ωs], props)
+end
+function Candidate(args::AbstractVector, param::Seq.ModSpec, props)
+    @assert length(param.ωs) == 1
+    @assert length(args) == length(param.Ωs) + 2
+    return Candidate(args[param.τ], args[param.ωs[1]],
+                     [args[Ω] for Ω in param.Ωs], props)
+end
+
+struct Candidates
+    candidates::Vector{Candidate}
+    meta::String
+end
+
+module CandidateSerialization
+
+import ..Candidate, ..Candidates
+
+import ProtoBuf as PB
+PB.default_values(::Type{Candidate}) = (;τ = 0.0, ω = 0.0, Ωs = Vector{Float64}(),
+                                        props = nothing)
+PB.field_numbers(::Type{Candidate}) = (;τ=1, ω=2, Ωs=3, props = 4)
+Base.:(==)(v1::Candidate, v2::Candidate) = v1.τ == v2.τ && v1.ω == v2.ω && v1.Ωs == v2.Ωs && v1.props == v2.props
+Base.hash(v::Candidate, h::UInt) = hash(v.τ, hash(v.ω, hash(v.Ωs, hash(v.props, hash(:Candidate, h)))))
+
+function PB.decode(d::PB.AbstractProtoDecoder, ::Type{<:Candidate})
+    τ = zero(Float64)
+    ω = zero(Float64)
+    Ωs = PB.BufferedVector{Float64}()
+    props = Ref{Union{Nothing,Seq.SolutionProperties}}(nothing)
+    while !PB.message_done(d)
+        field_number, wire_type = PB.decode_tag(d)
+        if field_number == 1
+            τ = PB.decode(d, Float64)
+        elseif field_number == 2
+            ω = PB.decode(d, Float64)
+        elseif field_number == 3
+            PB.decode!(d, wire_type, param)
+        elseif field_number == 4
+            PB.decode!(d, props)
+        else
+            PB.skip(d, wire_type)
+        end
     end
+    return Candidate(τ, ω, Ωs, props[])
+end
+
+function PB.encode(e::PB.AbstractProtoEncoder, x::Candidate)
+    initpos = position(e.io)
+    x.τ !== zero(Float64) && PB.encode(e, 1, x.τ)
+    x.ω !== zero(Float64) && PB.encode(e, 2, x.ω)
+    !isempty(x.Ωs) && PB.encode(e, 3, x.Ωs)
+    !isnothing(x.props) && PB.encode(e, 4, x.props)
+    return position(e.io) - initpos
+end
+function PB._encoded_size(x::Candidate)
+    encoded_size = 0
+    x.τ !== zero(Float64) && (encoded_size += PB._encoded_size(x.τ, 1))
+    x.ω !== zero(Float64) && (encoded_size += PB._encoded_size(x.ω, 2))
+    !isempty(x.Ωs) && (encoded_size += PB._encoded_size(x.Ωs, 3))
+    !isnothing(x.props) && (encoded_size += PB._encoded_size(x.props, 4))
+    return encoded_size
+end
+
+PB.default_values(::Type{Candidates}) = (;candidates = Vector{Candidate}(), meta = "")
+PB.field_numbers(::Type{Candidates}) = (;candidates = 1, meta = 2)
+Base.:(==)(v1::Candidates, v2::Candidates) =
+    v1.candidates == v2.candidates && v1.meta == v2.meta
+Base.hash(v::Candidates, h::UInt) = hash(v.candidates, hash(v.meta, hash(:Candidates, h)))
+
+function PB.decode(d::PB.AbstractProtoDecoder, ::Type{<:Candidates})
+    candidates = PB.BufferedVector{Candidate}()
+    meta = ""
+    while !PB.message_done(d)
+        field_number, wire_type = PB.decode_tag(d)
+        if field_number == 1
+            PB.decode!(d, candidates)
+        elseif field_number == 2
+            meta = PB.decode(d, String)
+        else
+            PB.skip(d, wire_type)
+        end
+    end
+    return Candidates(candidates[], meta)
+end
+
+function PB.encode(e::PB.AbstractProtoEncoder, x::Candidates)
+    initpos = position(e.io)
+    !isempty(x.candidates) && PB.encode(e, 1, x.candidates)
+    !isempty(x.meta) && PB.encode(e, 2, x.meta)
+    return position(e.io) - initpos
+end
+function PB._encoded_size(x::Candidates)
+    encoded_size = 0
+    !isempty(x.candidates) && (encoded_size += PB._encoded_size(x.candidates, 1))
+    !isempty(x.meta) && (encoded_size += PB._encoded_size(x.meta, 2))
+    return encoded_size
+end
+
 end
 
 struct PreOptimizer{NSeg,PreObj,Sum}
@@ -145,6 +239,8 @@ function opt_all_rounds!(pool::ThreadObjectPool{PreOpt}, nrounds,
     return candidates
 end
 
+const prefix = ARGS[1]
+
 params_file = "072125_goldparams_13ions.json"
 sysparams = open(params_file) do io
     read(io, GoldGates.SystemParams; format=:json)
@@ -159,3 +255,8 @@ const pre_pool = ThreadObjectPool() do
 end
 candidates = @time opt_all_rounds!(pre_pool, 100, Candidate[])
 @show length(candidates)
+
+open("$(prefix).binpb", "w") do io
+    encoder = PB.ProtoEncoder(io)
+    PB.encode(encoder, Candidates(candidates, ""))
+end
